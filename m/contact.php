43473028a2d9d17e08c263b52316fc458be510c7
AntoineAug <antoine@augusti.fr>
2011-10-28 22:26:47 +0200

    Contact sur la version mobile

--- m/contact.php
+++ m/contact.php
@@ -1,12 +1,16 @@
 <?php 
-include 'header.php'; ?>
+include 'header.php';
+include '../lang/'.$language.'/contact.php';
+?>
 <div class="post">
-<h1><img src="http://www.teen-quotes.com/images/icones/contact.png" class="icone" />Contact</h1>
-<img src="http://www.teen-quotes.com/images/icones/mail.png" class="icone" /><a href="mailto:contact@teen-quotes.com">Email : contact@teen-quotes.com</a><br>
-<br />
-<img src="http://www.teen-quotes.com/images/icones/antoine.png" class="icone" /><a href="http://www.antoine-augusti.fr" target="_blank">Antoine Augusti : http://www.antoine-augusti.fr</a><br>
-<br />
-<img src="http://www.pretty-web.com/images/icones/frog.png" class="icone"><a href="http://www.pretty-web.com" target="_blank">Partner : Pretty Web</a><br>
-<br />
+	<h1><img src="http://www.teen-quotes.com/images/icones/contact.png" class="icone" />Contact</h1>
+	<img src="http://www.teen-quotes.com/images/icones/mail.png" class="icone" /> <?php echo $about_website; ?> : <a href="mailto:contact@pretty-web.com">contact@pretty-web.com</a><br>
+	<br />
+	<img src="http://www.teen-quotes.com/images/icones/mail.png" class="icone" /> <?php echo $about_twitter_account; ?> : <a href="mailto:contact@teen-quotes.com">contact@teen-quotes.com</a><br>
+	<br />
+	<img src="http://www.teen-quotes.com/images/icones/antoine.png" class="icone" />Antoine Augusti - <?php echo $developer; ?> : <a href="http://www.antoine-augusti.fr" target="_blank">www.antoine-augusti.fr</a><br>
+	<br />
+	<img src="http://www.pretty-web.com/images/icones/frog.png" class="icone"><?php echo $partner; ?> : <a href="http://www.pretty-web.com" target="_blank">Pretty Web</a><br>
+	<br />
 </div>
 <?php include "footer.php"; ?>
