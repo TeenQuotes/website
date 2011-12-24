01229e88206a210de5bdc8774fac5cda4f723acf
AntoineAug <antoine@augusti.fr>
2011-12-24 23:43:09 +0100

    Texte admin ajax

--- ajax/admin_quote.php
+++ ajax/admin_quote.php
@@ -17,7 +17,7 @@ if (preg_match('/teen-quotes.com/', $_SERVER['SERVER_NAME']))
 		
 		if ($approve_quote) 
 			{
-			echo ''.$succes.' The author has been notified successfully !';
+			echo ''.$succes.' The quote has been added to the queue. The author will be notified';
 			}
 		}
 	else
@@ -36,7 +36,7 @@ if (preg_match('/teen-quotes.com/', $_SERVER['SERVER_NAME']))
 			{
 			$message = ''.$top_mail.' Hello <font color="#5C9FC0"><b>'.$name_auteur.'</b></font> !<br><br />Your quote has been <font color="#5C9FC0"><b>rejected</b></font> recently by a member of our team...<br><div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br><br /><a href="http://www.teen-quotes.com" target="_blank">#'.$id_quote.'</a><span style="float:right">by <a href="http://www.teen-quotes.com/user-'.$auteur_id.'" target="_blank">'.$name_auteur.'</a> on '.$date_quote.'</span></div>Sincerely,<br><b>The Teen Quotes Team</b><br /><br /><br /><div style="border-top:1px dashed #CCCCCC"></div><br /><br />VERSION FRANCAISE :<br /><br />Bonjour <font color="#5C9FC0"><b>'.$name_auteur.'</b></font> !<br><br />Votre citation a été <font color="#5C9FC0"><b>rejetée</b></font> récemment par un membre de notre équipe...<br><div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br><br /><a href="http://www.teen-quotes.com" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://www.teen-quotes.com/user-'.$auteur_id.'" target="_blank">'.$name_auteur.'</a> le '.$date_quote.'</span></div>Cordialement,<br><b>The Teen Quotes Team</b> '.$end_mail.'';
 			$mail = mail($email_auteur, "Quote rejected", $message, $headers); 
-			echo ''.$succes.' The quote has been added to the queue. The author will be notified';
+			echo ''.$succes.' The author has been notified successfully';
 			}
 		}
 	}		
