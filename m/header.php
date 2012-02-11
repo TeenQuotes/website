e1998f4d96720d7507510de42411899222dac410
AntoineAug <antoine@augusti.fr>
2012-02-11 18:59:00 +0100

    Correction problème d'affichage du site mobile

--- m/header.php
+++ m/header.php
@@ -114,7 +114,8 @@ else
 		<meta name="location" content="France, FRANCE"/> 
 		<meta name="expires" content="never"/> 
 		<meta name="Distribution" content="Global"/> 
-		<meta name="Audience" content="General"/> 
+		<meta name="Audience" content="General"/>
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;"/>
 		<meta http-equiv="Content-Language" content="en,fr" /> 
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		
