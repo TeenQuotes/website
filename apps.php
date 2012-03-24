6d46fdb82b4b58f8c57d6641cd64777098a16005
AntoineAug <antoine@augusti.fr>
2012-03-24 11:12:22 +0100

    Page téléchargement des apps

---
+++ apps.php
@@ -0,0 +1,23 @@
+<?php 
+include 'header.php';
+include 'lang/'.$language.'/apps.php';
+
+echo '
+<div class="post">
+	<h1><img src="http://'.$domaine.'/images/icones/mobile.png" class="icone" />'.$applications.'</h1>
+	<div class="grey_post">
+	'.$text_applications.'
+	</div>
+	
+	<div class="img_apps left">
+		<a href="'.$link_app_iphone.'" target="_blank"><img src="http://'.$domaine.'/images/icones/app_iphone.png" class="apps" /></a>
+	</div>
+	<div class="img_apps right">
+		<a href="'.$link_app_android.'" target="_blank"><img src="http://'.$domaine.'/images/icones/app_android.png" class="apps" /></a>
+	</div>
+	<div class="clear"></div>
+</div>
+';
+
+include "footer.php"; 
+?>
