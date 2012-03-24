af5593429d45b6b5b283c8b9feb5d0f4e7c5a9f8
AntoineAug <antoine@augusti.fr>
2012-03-24 19:06:31 +0100

    Page pour les applications

--- apps.php
+++ apps.php
@@ -10,10 +10,10 @@ echo '
 	</div>
 	
 	<div class="img_apps left">
-		<a href="'.$link_app_iphone.'" target="_blank"><img src="http://'.$domaine.'/images/icones/app_iphone.png" class="apps" /></a>
+		<a href="'.$link_app_iphone.'" target="_blank"><img src="http://'.$domaine.'/images/icones/app_iphone.png" class="apps" /><br /><img src="http://'.$domaine.'/images/icones/app_store_'.$language.'.png" class="download_app" /></a>
 	</div>
 	<div class="img_apps right">
-		<a href="'.$link_app_android.'" target="_blank"><img src="http://'.$domaine.'/images/icones/app_android.png" class="apps" /></a>
+		<a href="'.$link_app_android.'" target="_blank"><img src="http://'.$domaine.'/images/icones/app_android.png" class="apps" /><br /><img src="http://'.$domaine.'/images/icones/android_market_'.$language.'.png" class="download_app" /></a>
 	</div>
 	<div class="clear"></div>
 </div>
