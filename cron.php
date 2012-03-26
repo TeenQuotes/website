95a7b8b01c97f307842bea091a7ceee5032c8d07
AntoineAug <antoine@augusti.fr>
2012-03-26 20:02:49 +0200

    Refonte du code d'envoi de la newsletter

--- cron.php
+++ cron.php
@@ -5,33 +5,52 @@ require "kernel/fonctions.php";
 $db = mysql_connect($host, $user, $pass)  or die('Erreur de connexion '.mysql_error());
 mysql_select_db($user,$db)  or die('Erreur de selection '.mysql_error()); 
 
-$query_monday=mysql_fetch_array(mysql_query("SELECT send_mail_monday FROM config WHERE id='1'"));
-$send_monday=$query_monday['send_mail_monday'];
+$query_monday = mysql_fetch_array(mysql_query("SELECT send_mail_monday FROM config WHERE id='1'"));
+$send_monday = $query_monday['send_mail_monday'];
 
 $jour=date("D");
 
-if ($send_monday=="0" AND $jour=="Mon") 
+if ($send_monday == "0" AND $jour == "Mon") 
 	{ // ENVOI DE MAIL LE LUNDI
-	$message = "$top_mail";
+	$message = ''.$top_mail.'';
 	$message.= MailRandomQuote(15);
-	$message.= "$end_mail";
+	$message.= ''.$end_mail.'';
+	
+	echo 'Envoi de la newsletter';
+	
+	$today = date("d/m/Y");
+	$i = '0';
+	$txt_file = 'Newsletter on '.$today.'\r\n\n';
 
 
-	$query=mysql_query("SELECT email,code FROM newsletter");
+	$query = mysql_query("SELECT email, code FROM newsletter");
 
 	while ($donnees=mysql_fetch_array($query)) 
 		{
-		$email=$donnees['email'];
-		$code=$donnees['code'];
-		$unsuscribe= '<br /><span style="font-size:80%">This email was adressed to you ('.$email.') because you are subscribed to our newsletter. If you want to unsuscribe, please follow <a href="http://www.teen-quotes.com/newsletter.php?action=unsuscribe&email='.$email.'&code='.$code.'" target="_blank"> this link</a></span>';
-		$mail = mail ("$email", "Newsletter", $message.$unsuscribe, $headers);
+		$email = $donnees['email'];
+		$code = $donnees['code'];
+		$unsuscribe = '<br /><span style="font-size:80%">This email was adressed to you ('.$email.') because you are subscribed to our newsletter. If you want to unsuscribe, please follow <a href="http://www.teen-quotes.com/newsletter.php?action=unsuscribe&email='.$email.'&code='.$code.'" target="_blank"> this link</a></span>';
+		$mail = mail ($email, "Newsletter", $message.$unsuscribe, $headers);
+		if ($mail)
+			{
+			$i++;
+			$txt_file .= '#'.$i.' : '.$email.' - '.$code.' \r';
+			}
 		}
+	$monfichier = fopen('files/compteur_email_hebdomadaire.txt', 'r+'); // Ouverture du fichier
+	fseek($monfichier, 0); // On remet le curseur au début du fichier
+	fputs($monfichier, $txt_file); // On écrit le nouveau nombre de pages vues
+	fclose($monfichier);
 		
-	$update=mysql_query("UPDATE config SET send_mail_monday='1' WHERE id='1'");
+	$update = mysql_query("UPDATE config SET send_mail_monday='1' WHERE id='1'");
+	}
+else
+	{
+	echo 'Newsletter déjà envoyée';
 	}
 
 
-if ($send_monday=="1" AND $jour=="Tue") 
+if ($send_monday == "1" AND $jour == "Tue") 
 	{ // RESET COMPTEUR MARDI
 	$update=mysql_query("UPDATE config SET send_mail_monday='0' WHERE id='1'");
 	}
