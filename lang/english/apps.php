28971980440c8c42083c49b9b9dfd6c537cd9897
AntoineAug <antoine@augusti.fr>
2012-04-03 22:59:57 +0200

    Commit en retard : traduction pour la page des apps

---
+++ lang/english/apps.php
@@ -0,0 +1,15 @@
+<?php
+$applications = 'Applications';
+$text_applications = '
+Do not ever leave Teen Quotes. Free, easy to use and fast, this application offers the website\'s best functionalities.<br>
+<br />
+- Be warned when new quotes have been published<br>
+- Submit quotes and add comments<br>
+- Share on Facebook, Twitter and via email<br>
+- Add quotes to your favorites<br>
+<br />
+<br />
+Just click on the type of mobile you have in order to download the application !';
+
+$app_iphone_not_available = '<br /><br /><img src="http://teen-quotes.com/images/icones/erreur.png" class="mini_icone" alt="Icone" />The iPhone application is not yet available.<br>';
+$app_android_not_available = '<br /><br /><img src="http://teen-quotes.com/images/icones/erreur.png" class="mini_icone" alt="Icone" />The Android application is not yet available.<br>';
---
+++ lang/french/apps.php
@@ -0,0 +1,15 @@
+<?php
+$applications = 'Applications';
+$text_applications = '
+Ne quittez plus jamais Teen Quotes. Gratuite, facile à utiliser et rapide, cette application offre les meilleures fonctionnalités du site.<br>
+<br />
+- Soyez avertis lorsque des citations ont été ajoutées<br>
+- Ajoutez des citations et des commentaires<br>
+- Partager sur Facebook, Twitter et via email<br>
+- Ajoutez des citations à vos favoris<br>
+<br />
+<br />
+Cliquez simplement sur le type de mobile que vous possédez pour télécharger l\'application !';
+
+$app_iphone_not_available = '<br /><br /><img src="http://teen-quotes.com/images/icones/erreur.png" class="mini_icone" alt="Icone" />L\'application iPhone n\'est pas encore disponible.<br>';
+$app_android_not_available = '<br /><br /><img src="http://teen-quotes.com/images/icones/erreur.png" class="mini_icone" alt="Icone" />L\'application Android n\'est pas encore disponible.<br>';
