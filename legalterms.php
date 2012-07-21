0f2f516cc9126fea769877a3ce8b8cb4d510d053
Antoine Augusti <antoine@augusti.fr>
2012-07-21 22:32:03 +0200

    Syntaxe du code

--- legalterms.php
+++ legalterms.php
@@ -4,10 +4,9 @@ include 'lang/'.$language.'/legalterms.php';
 
 echo '
 <div class="post">
-<h1><img src="http://'.$domaine.'/images/icones/balance.png" class="icone" />'.$legal_terms.'</h1>
-'.$texte_legal.'
-</div>
-';
+	<h1><img src="http://'.$domaine.'/images/icones/balance.png" class="icone" />'.$legal_terms.'</h1>
+	'.$texte_legal.'
+</div>';
 
-include "footer.php"; 
+include "footer.php";
 ?>
