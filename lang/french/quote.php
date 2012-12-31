ee11a21330da2fff01ca7b344332ca6075ad6a4f
Antoine Augusti <antoine@augusti.fr>
2012-12-31 15:57:30 +0100

    Language:quote

--- lang/french/quote.php
+++ lang/french/quote.php
@@ -1,9 +1,9 @@
 <?php
 $comment = "commentaire";
 $warning_comments = "Ecrivez ici votre mot doux. Soyez respectueux.";
-$no_comments = "Il n'y a pas encore de commentaires pour cette citation";
+$no_comments = "Il n'y a pas encore de commentaires pour cette citation. Soyez le premier à ajouter le votre !";
 $comment_too_long = "Votre commentaire est trop long ! Il doit faire un maximum de 450 caractères";
-$comment_add_succes = "$succes Votre commentaire a été ajouté avec succès !<br/><br/>Vous allez être redirigé dans un instant...";
+$comment_add_succes = $succes." Votre commentaire a été ajouté avec succès !<br/><br/>Vous allez être redirigé dans un instant...";
 $not_complete = "Tous les champs ne sont pas remplis !";
 $comment_added_on_quote = "Commentaire ajouté sur votre citation";
 $refresh = "Voir une autre citation";
