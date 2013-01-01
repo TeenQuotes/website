db0fa71a11548d737ab52109722d121890c2eba6
Antoine Augusti <antoine@augusti.fr>
2013-01-01 19:45:03 +0100

    Update contact page for mobile

--- m/contact.php
+++ m/contact.php
@@ -8,15 +8,11 @@ if (empty($action))
 {
 	echo '
 	<div class="post">
-		<h2><img src="http://'.$domaine.'/images/icones/contact.png" class="icone" />Contact</h2>
+		<h2><img src="http://'.$domaine.'/images/icones/mail.png" class="icone" />Contact</h2>
 		<div class="grey_post">
 			<img src="http://'.$domaine.'/images/icones/mail.png" class="icone" /> '.$about_website.' : <a href="mailto:support@teen-quotes.com">support@teen-quotes.com</a><br/>
 			<br/>
 			<img src="http://'.$domaine.'/images/icones/mail.png" class="icone" /> '.$about_twitter_account.' : <a href="mailto:contact@teen-quotes.com">contact@teen-quotes.com</a><br/>
-			<br/>
-			<img src="http://'.$domaine.'/images/icones/antoine.png" class="icone" />Antoine Augusti - '.$developer.' : <a href="http://www.antoine-augusti.fr" target="_blank">www.antoine-augusti.fr</a><br/>
-			<br/>
-			<img src="http://www.pretty-web.com/images/icones/frog.png" class="icone">'.$partner.' : <a href="http://www.pretty-web.com" target="_blank">Pretty Web</a><br/>
 		</div>
 
 		<h2><img src="http://'.$domaine.'/images/icones/staff.png" class="icone" />'.$team.'</h2>
