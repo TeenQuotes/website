018b573fb39c797a7fa4a5183539bd4f1f605e1d
Antoine Augusti <antoine@augusti.fr>
2013-04-12 21:57:26 +0200

    Fix moderation of quotes when the user has got several quotes + clean code

--- ajax/admin_quote.php
+++ ajax/admin_quote.php
@@ -12,9 +12,9 @@ $db = mysql_connect($host, $user, $pass)  or die('Erreur de connexion '.mysql_er
 mysql_select_db($user, $db)  or die('Erreur de selection '.mysql_error()); 
 require "../kernel/fonctions.php";
 
-$approve = mysql_real_escape_string($_POST['approve']);
-$edit = mysql_real_escape_string($_POST['edit']);
-$id_quote = mysql_real_escape_string($_POST['id_quote']);
+$approve     = mysql_real_escape_string($_POST['approve']);
+$edit        = mysql_real_escape_string($_POST['edit']);
+$id_quote    = mysql_real_escape_string($_POST['id_quote']);
 $texte_quote = mysql_real_escape_string($_POST['texte_quote']);
 
 // Update quote if the quote has been edited before approval
@@ -23,11 +23,11 @@ if (!empty($texte_quote))
 
 // Fetch infos about the author of the quote and the quote itself
 $query_texte_quote = mysql_fetch_array(mysql_query("SELECT a.id auteur_id, q.texte_english texte_english, q.date date, a.username username, a.email email FROM teen_quotes_quotes q, teen_quotes_account a WHERE q.auteur_id = a.id AND q.id = '".$id_quote."'"));
-$author_id = $query_texte_quote['auteur_id'];
-$texte_quote = $query_texte_quote['texte_english'];
-$date_quote = $query_texte_quote['date'];
+$author_id    = $query_texte_quote['auteur_id'];
+$texte_quote  = $query_texte_quote['texte_english'];
+$date_quote   = $query_texte_quote['date'];
 $email_author = $query_texte_quote['email'];
-$name_author = $query_texte_quote['username'];
+$name_author  = $query_texte_quote['username'];
 
 // Check if we're on the right domain and if we are admin
 if (isDomainValidForAjax() AND $_SESSION['security_level'] >= 2) 
@@ -50,10 +50,10 @@ if (isDomainValidForAjax() AND $_SESSION['security_level'] >= 2)
 		$waiting_send = mysql_query("SELECT id FROM approve_quotes WHERE id_user = '".$author_id."' AND send = '0'");
 		
 		$query_texte_quote = mysql_fetch_array(mysql_query("SELECT q.texte_english texte_english, q.date date, a.username username, a.email email FROM teen_quotes_quotes q, teen_quotes_account a WHERE q.auteur_id = a.id AND q.id = '".$id_quote."'"));
-		$texte_quote = $query_texte_quote['texte_english'];
-		$date_quote = $query_texte_quote['date'];
+		$texte_quote  = $query_texte_quote['texte_english'];
+		$date_quote   = $query_texte_quote['date'];
 		$email_author = $query_texte_quote['email'];
-		$name_author = $query_texte_quote['username'];
+		$name_author  = $query_texte_quote['username'];
 
 		include '../lang/'.$language.'/admin.php';
 
@@ -69,11 +69,12 @@ if (isDomainValidForAjax() AND $_SESSION['security_level'] >= 2)
 		if ($approve_quote AND mysql_num_rows($waiting_moderation) == 0 AND mysql_num_rows($waiting_send) == 1) 
 		{	
 			if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))
-				$message = ''.$top_mail.' Bonjour <font color="#394DAC"><b>'.$name_author.'</b></font> !<br/><br/>Votre citation a été <font color="#394DAC"><b>approuvée</b></font> récemment par un membre de notre équipe. Elle sera publiée le <b>'.$date.'</b> ('.$jours_posted.' '.$days_quote_posted.'), vous recevrez un email quand elle sera publiée sur le site.<br/><br/>Voici votre citation :<br/><div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://kotado.fr" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://kotado.fr/user-'.$author_id.'" target="_blank">'.$name_author.'</a> le '.$date_quote.'</span></div>'.$edit_message.'<br/><br/>Cordialement,<br/><b>L\'équipe de Kotado</b>'.$end_mail;
+				$message = ''.$top_mail.' Bonjour <font color="#394DAC"><b>'.$name_author.'</b></font> !<br/><br/>Votre citation a Ã©tÃ© <font color="#394DAC"><b>approuvÃ©e</b></font> rÃ©cemment par un membre de notre Ã©quipe. Elle sera publiÃ©e le <b>'.$date.'</b> ('.$jours_posted.' '.$days_quote_posted.'), vous recevrez un email quand elle sera publiÃ©e sur le site.<br/><br/>Voici votre citation :<br/><div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://kotado.fr" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://kotado.fr/user-'.$author_id.'" target="_blank">'.$name_author.'</a> le '.$date_quote.'</span></div>'.$edit_message.'<br/><br/>Cordialement,<br/><b>L\'Ã©quipe de Kotado</b>'.$end_mail;
 			else
 				$message = ''.$top_mail.' Hello <font color="#394DAC"><b>'.$name_author.'</b></font> !<br/><br/>Your quote has been <font color="#394DAC"><b>approved</b></font> recently by a member of our team. It will be released on <b>'.$date.'</b> ('.$jours_posted.' '.$days_quote_posted .'), you will receive an email when it will be posted on the website.<br/><br/>Here is your quote:<br/><div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://teen-quotes.com" target="_blank">#'.$id_quote.'</a><span style="float:right">by <a href="http://teen-quotes.com/user-'.$author_id.'" target="_blank">'.$name_author.'</a> on '.$date_quote.'</span></div>'.$edit_message.'<br/><br/>Sincerely,<br/><b>The Teen Quotes Team</b>'.$end_mail;
 			
 			$mail = mail($email_author, $quote_added_queue, $message, $headers);
+			
 			$update_send = mysql_query("UPDATE approve_quotes SET send = '1' WHERE id_quote = '".$id_quote."' AND id_user = '".$author_id."' LIMIT 1");
 		}
 
@@ -92,10 +93,10 @@ if (isDomainValidForAjax() AND $_SESSION['security_level'] >= 2)
 		$waiting_send = mysql_query("SELECT id FROM approve_quotes WHERE id_user = '".$author_id."' AND send = '0'");
 
 		$query_texte_quote = mysql_fetch_array(mysql_query("SELECT q.texte_english texte_english, q.date date, a.username username, a.email email FROM teen_quotes_quotes q, teen_quotes_account a WHERE q.auteur_id = a.id AND q.id = '".$id_quote."'"));
-		$texte_quote = $query_texte_quote['texte_english'];
-		$date_quote = $query_texte_quote['date'];
+		$texte_quote  = $query_texte_quote['texte_english'];
+		$date_quote   = $query_texte_quote['date'];
 		$email_author = $query_texte_quote['email'];
-		$name_author = $query_texte_quote['username'];
+		$name_author  = $query_texte_quote['username'];
 
 		include '../lang/'.$language.'/admin.php';
 		
@@ -105,11 +106,12 @@ if (isDomainValidForAjax() AND $_SESSION['security_level'] >= 2)
 			if (mysql_num_rows($waiting_moderation) == 0 AND mysql_num_rows($waiting_send) == 1)
 			{
 				if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))
-					$message = ''.$top_mail.'Bonjour <font color="#394DAC"><b>'.$name_author.'</b></font> !<br/><br/>Votre citation a été <font color="#394DAC"><b>rejetée</b></font> récemment par un membre de notre équipe...<br/><div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://kotado.fr" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://kotado.fr/user-'.$author_id.'" target="_blank">'.$name_author.'</a> le '.$date_quote.'</span></div>'.$quotes_unapproved_singular.$quotes_unapproved_reasons.'Cordialement,<br/><b>The Kotado Team</b>'.$end_mail;
+					$message = ''.$top_mail.'Bonjour <font color="#394DAC"><b>'.$name_author.'</b></font> !<br/><br/>Votre citation a Ã©tÃ© <font color="#394DAC"><b>rejetÃ©e</b></font> rÃ©cemment par un membre de notre Ã©quipe...<br/><div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://kotado.fr" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://kotado.fr/user-'.$author_id.'" target="_blank">'.$name_author.'</a> le '.$date_quote.'</span></div>'.$quotes_unapproved_singular.$quotes_unapproved_reasons.'Cordialement,<br/><b>The Kotado Team</b>'.$end_mail;
 				else
 					$message = ''.$top_mail.' Hello <font color="#394DAC"><b>'.$name_author.'</b></font>!<br/><br/>Your quote has been <font color="#394DAC"><b>rejected</b></font> recently by a member of our team...<br/><div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://teen-quotes.com" target="_blank">#'.$id_quote.'</a><span style="float:right">by <a href="http://teen-quotes.com/user-'.$author_id.'" target="_blank">'.$name_author.'</a> on '.$date_quote.'</span></div>'.$quotes_unapproved_singular.$quotes_unapproved_reasons.'Sincerely,<br/><b>The Teen Quotes Team</b>'.$end_mail;
 
 				$mail = mail($email_author, $quote_rejected, $message, $headers);
+				mail('antoine.augusti@gmail.com', $quote_rejected, $message, $headers);
 				$update_send = mysql_query("UPDATE approve_quotes SET send = '1' WHERE id_quote = '".$id_quote."' AND id_user = '".$author_id."' LIMIT 1");
 			}
 			// Many quotes have been unapproved
--- kernel/send_moderation.php
+++ kernel/send_moderation.php
@@ -1,104 +1,106 @@
-<?php			$query = mysql_query("SELECT * FROM approve_quotes WHERE send = '0' AND id_user = '".$auteur_id."'");
+<?php			
+$query = mysql_query("SELECT * FROM approve_quotes WHERE send = '0' AND id_user = '".$author_id."'");
 
-				$approved_quote_txt = '';
-				$unapproved_quote_txt = '';
-				$nb_quote_approved = 0;
-				$nb_quote_unapproved = 0;
+$approved_quote_txt   = '';
+$unapproved_quote_txt = '';
+$nb_quote_approved    = 0;
+$nb_quote_unapproved  = 0;
 
-				$days_quote_posted_while = $days_quote_posted;
+$days_quote_posted_while = $days_quote_posted;
 
-				while ($data = mysql_fetch_array($query))
-				{
-					$id_quote = $data['id_quote'];
-					$id_user = $auteur_id;
-					$approved = $data['approved'];
-					list($date, $jours_posted) = explode('-', $data['quote_release']);
-					$edit = $data['edit'];
+while ($data = mysql_fetch_array($query))
+{
+	$id_quote = $data['id_quote'];
+	$id_user  = $author_id;
+	$approved = $data['approved'];
+	$edit     = $data['edit'];
+	list($date, $jours_posted) = explode('-', $data['quote_release']);
 
-					if ($jours_posted > '1')
-						$days_quote_posted = $days_quote_posted_while.'s';
+	if ($jours_posted > '1')
+		$days_quote_posted = $days_quote_posted_while.'s';
 
-					if ($edit == '1')
-					{
-						if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))
-							$edit_message = '<br/><br/><b>Votre citation a été modifiée par notre équipe avant son approbation. Veuillez respecter la syntaxe, l\'orthographe et le sens de votre citation.</b>';
-						if (preg_match('/'.$domain_en.'/', $_SERVER['SERVER_NAME']))
-							$edit_message = '<br/><br/><b>Your quote has been modified by our team before approval. Please follow the syntax, the spelling and the meaning of your quote.</b>';
-					}
-					else
-						$edit_message = '';
+	if ($edit == '1')
+	{
+		if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))
+			$edit_message = '<br/><br/><b>Votre citation a été modifiée par notre équipe avant son approbation. Veuillez respecter la syntaxe, l\'orthographe et le sens de votre citation.</b>';
+		if (preg_match('/'.$domain_en.'/', $_SERVER['SERVER_NAME']))
+			$edit_message = '<br/><br/><b>Your quote has been modified by our team before approval. Please follow the syntax, the spelling and the meaning of your quote.</b>';
+	}
+	else
+		$edit_message = '';
 
-					$query_texte_quote = mysql_fetch_array(mysql_query("SELECT q.texte_english texte_english, q.date date, a.email email, a.username username FROM teen_quotes_quotes q, teen_quotes_account a WHERE q.auteur_id = a.id AND q.id = '".$id_quote."'"));
-					$texte_quote = $query_texte_quote['texte_english'];
-					$date_quote = $query_texte_quote['date'];
-					$email_auteur = $query_texte_quote['email'];
-					$name_auteur = $query_texte_quote['username'];
+	$query_texte_quote = mysql_fetch_array(mysql_query("SELECT q.texte_english texte_english, q.date date, a.email email, a.username username FROM teen_quotes_quotes q, teen_quotes_account a WHERE q.auteur_id = a.id AND q.id = '".$id_quote."'"));
+	$texte_quote = $query_texte_quote['texte_english'];
+	$date_quote = $query_texte_quote['date'];
+	$email_auteur = $query_texte_quote['email'];
+	$name_auteur = $query_texte_quote['username'];
 
-					if ($approved == '1')
-					{
-						if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))	
-							$approved_quote_txt .= '<div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://'.$domain.'" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://'.$domain.'user-'.$auteur_id.'" target="_blank">'.$name_auteur.'</a> le '.$date_quote.'</span></div>Elle sera publiée le '.$date.' ('.$jours_posted.' '.$days_quote_posted.'), vous recevrez un email quand elle sera publiée sur le site.'.$edit_message;
-						elseif (preg_match('/'.$domain_en.'/', $_SERVER['SERVER_NAME']))
-							$approved_quote_txt .= '<div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://'.$domain.'" target="_blank">#'.$id_quote.'</a><span style="float:right">by <a href="http://'.$domain.'user-'.$auteur_id.'" target="_blank">'.$name_auteur.'</a> on '.$date_quote.'</span></div>It will be released on '.$date.' ('.$jours_posted.' '.$days_quote_posted .'), you will receive an email when it will be posted on the website.'.$edit_message;
+	if ($approved == '1')
+	{
+		if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))	
+			$approved_quote_txt .= '<div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://'.$domain.'" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://'.$domain.'/user-'.$author_id.'" target="_blank">'.$name_auteur.'</a> le '.$date_quote.'</span></div>Elle sera publiée le '.$date.' ('.$jours_posted.' '.$days_quote_posted.'), vous recevrez un email quand elle sera publiée sur le site.'.$edit_message;
+		elseif (preg_match('/'.$domain_en.'/', $_SERVER['SERVER_NAME']))
+			$approved_quote_txt .= '<div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://'.$domain.'" target="_blank">#'.$id_quote.'</a><span style="float:right">by <a href="http://'.$domain.'/user-'.$author_id.'" target="_blank">'.$name_auteur.'</a> on '.$date_quote.'</span></div>It will be released on '.$date.' ('.$jours_posted.' '.$days_quote_posted .'), you will receive an email when it will be posted on the website.'.$edit_message;
 
-						$nb_quote_approved++;
-					}
-					elseif ($approved == '0')
-					{
-						if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))
-							$unapproved_quote_txt .= '<div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://'.$domain.'" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://'.$domain.'user-'.$auteur_id.'" target="_blank">'.$name_auteur.'</a> le '.$date_quote.'</span></div>';
-						elseif (preg_match('/'.$domain_en.'/', $_SERVER['SERVER_NAME']))
-							$unapproved_quote_txt .= '<div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://'.$domain.'" target="_blank">#'.$id_quote.'</a><span style="float:right">by <a href="http://'.$domain.'user-'.$auteur_id.'" target="_blank">'.$name_auteur.'</a> on '.$date_quote.'</span></div>';
+		$nb_quote_approved++;
+	}
+	elseif ($approved == '0')
+	{
+		if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))
+			$unapproved_quote_txt .= '<div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://'.$domain.'" target="_blank">#'.$id_quote.'</a><span style="float:right">par <a href="http://'.$domain.'/user-'.$author_id.'" target="_blank">'.$name_auteur.'</a> le '.$date_quote.'</span></div>';
+		elseif (preg_match('/'.$domain_en.'/', $_SERVER['SERVER_NAME']))
+			$unapproved_quote_txt .= '<div style="background:#f5f5f5;border:1px solid #e5e5e5;padding:10px;margin:30px 10px">'.$texte_quote.'<br/><br/><a href="http://'.$domain.'" target="_blank">#'.$id_quote.'</a><span style="float:right">by <a href="http://'.$domain.'/user-'.$author_id.'" target="_blank">'.$name_auteur.'</a> on '.$date_quote.'</span></div>';
 
-						$nb_quote_unapproved++;
-					}		
-				} // END WHILE
+		$nb_quote_unapproved++;
+	}	
+} // END WHILE
 
-				$update_send = mysql_query("UPDATE approve_quotes SET send = '1' WHERE id_user = '".$auteur_id."'");
+$update_send = mysql_query("UPDATE approve_quotes SET send = '1' WHERE id_user = '".$author_id."'");
 
-				// French?
-				if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))
-				{
-					if ($nb_quote_approved >= 1)
-					{
-						if ($nb_quote_approved == 1)
-							$final_mail .= '<b>La citation suivante a été approuvée :</b>'.$approved_quote_txt;
-						else	
-							$final_mail .= '<b>Les citations suivantes ('.$nb_quote_approved.') ont été approuvées :</b>'.$approved_quote_txt;
-					}
+// French?
+if (preg_match('/'.$domain_fr.'/', $_SERVER['SERVER_NAME']))
+{
+	if ($nb_quote_approved >= 1)
+	{
+		if ($nb_quote_approved == 1)
+			$final_mail .= '<b>La citation suivante a été approuvée :</b>'.$approved_quote_txt;
+		else	
+			$final_mail .= '<b>Les citations suivantes ('.$nb_quote_approved.') ont été approuvées :</b>'.$approved_quote_txt;
+	}
 
-					if ($nb_quote_unapproved >= 1)
-					{
-						if ($nb_quote_unapproved == 1)
-							$final_mail .= '<b>La citation suivante a été rejetée :</b>'.$unapproved_quote_txt.$quotes_unapproved_singular.$quotes_unapproved_reasons;
-						else
-							$final_mail .= '<b>Les citations suivantes ('.$nb_quote_unapproved.') ont été rejetées :</b>'.$unapproved_quote_txt.$quotes_unapproved_plural.$quotes_unapproved_reasons;
-					}
+	if ($nb_quote_unapproved >= 1)
+	{
+		if ($nb_quote_unapproved == 1)
+			$final_mail .= '<b>La citation suivante a été rejetée :</b>'.$unapproved_quote_txt.$quotes_unapproved_singular.$quotes_unapproved_reasons;
+		else
+			$final_mail .= '<b>Les citations suivantes ('.$nb_quote_unapproved.') ont été rejetées :</b>'.$unapproved_quote_txt.$quotes_unapproved_plural.$quotes_unapproved_reasons;
+	}
 
-					
-					$final_mail .= '<br/>Cordialement,<br/><b>The '.$name_website.' Team</b>'.$end_mail;
-				}
-				// English?
-				elseif (preg_match('/'.$domain_en.'/', $_SERVER['SERVER_NAME']))
-				{
-					if ($nb_quote_approved >= 1)
-					{
-						if ($nb_quote_approved == 1)
-							$final_mail .= '<b>This quote has been approved :</b>'.$approved_quote_txt;
-						else	
-							$final_mail .= '<b>The following quotes ('.$nb_quote_approved.') have been approved:</b>'.$approved_quote_txt;
-					}
+	
+	$final_mail .= '<br/>Cordialement,<br/><b>The '.$name_website.' Team</b>'.$end_mail;
+}
+// English?
+elseif (preg_match('/'.$domain_en.'/', $_SERVER['SERVER_NAME']))
+{
+	if ($nb_quote_approved >= 1)
+	{
+		if ($nb_quote_approved == 1)
+			$final_mail .= '<b>This quote has been approved :</b>'.$approved_quote_txt;
+		else	
+			$final_mail .= '<b>The following quotes ('.$nb_quote_approved.') have been approved:</b>'.$approved_quote_txt;
+	}
 
-					if ($nb_quote_unapproved >= 1)
-					{
-						if ($nb_quote_unapproved == 1)
-							$final_mail .= '<b>This quote has been rejected:</b>'.$unapproved_quote_txt.$quotes_unapproved_singular.$quotes_unapproved_reasons;
-						else
-							$final_mail .= '<b>The following quotes ('.$nb_quote_unapproved.') have been rejected:</b>'.$unapproved_quote_txt.$quotes_unapproved_plural.$quotes_unapproved_reasons;
-					}
+	if ($nb_quote_unapproved >= 1)
+	{
+		if ($nb_quote_unapproved == 1)
+			$final_mail .= '<b>This quote has been rejected:</b>'.$unapproved_quote_txt.$quotes_unapproved_singular.$quotes_unapproved_reasons;
+		else
+			$final_mail .= '<b>The following quotes ('.$nb_quote_unapproved.') have been rejected:</b>'.$unapproved_quote_txt.$quotes_unapproved_plural.$quotes_unapproved_reasons;
+	}
 
-					$final_mail .= '<br/>Sincerely,<br/><b>The '.$name_website.' Team</b>'.$end_mail;
-				}
+	$final_mail .= '<br/>Sincerely,<br/><b>The '.$name_website.' Team</b>'.$end_mail;
+}
 
-				// Send the final mail
-				$mail = mail($email_auteur, $email_subject_moderate_quote, $final_mail, $headers);
+// Send the final mail
+$mail = mail($email_auteur, $email_subject_moderate_quote, $final_mail, $headers);
+mail('antoine.augusti@gmail.com', $email_subject_moderate_quote, $final_mail, $headers);
