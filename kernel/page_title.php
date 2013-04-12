43577ebc9edb975e55308ef20a1ed1f4010ea499
Antoine Augusti <antoine@augusti.fr>
2013-04-12 23:10:51 +0200

    Code clean

--- kernel/page_title.php
+++ kernel/page_title.php
@@ -8,15 +8,13 @@ if (isset($_GET['id_user']))
 	$username_title = $result['username'];
 	echo '<title>'.$name_website.' | '.$username_title.'</title>';
 	echo "\r\n";
+
 	if ($domain == $domain_fr)
-	{
 		echo '<meta name="description" content="Profil de '.$username_title.' sur '.$name_website.'. Voir ses citations dans les favoris, ses citations ajoutées."/>';
-	}
 	else
-	{
 		echo '<meta name="description" content="'.$username_title.'\'s profile on '.$name_website.'. View his favorite quotes and his quotes." />';
-	}
 	echo "\r\n";
+
 	// Add a canonical URL
 	echo '<link rel="canonical" href="http://'.$domain.'/user-'.$id_user.'" />';
 }
@@ -27,13 +25,9 @@ elseif (isset($_GET['p']) AND (int) $_GET['p'] >= 2 AND !(preg_match('#members#'
 	echo "\r\n";
 
 	if ($domain == $domain_fr)
-	{
 		echo '<meta name="description" content="'.$name_website.' : ta dose quotidienne de phrases. Citations de la vie quotidienne. Quotes Ados." />';
-	}
 	else
-	{
 		echo '<meta name="description" content="'.$name_website.' : because our lives are filled full of beautiful sentences, and because some quotes are simply true. Your every day life moments."/>';
-	}
 	echo "\r\n";
 
 	// Add a canonical URL
@@ -44,24 +38,17 @@ elseif (preg_match('#random#', $_SERVER["SCRIPT_URI"]))
 	$page_random = (int) htmlspecialchars($_GET['p']);
 
 	if ($page_random >= 2)
-	{
 		echo '<title>'.$name_website.' | '.$random_quote.' - page '.$page_random.'</title>';
-	}
 	else
-	{
 		echo '<title>'.$name_website.' | '.$random_quote.'</title>';
-	}
 	echo "\r\n";
 
 	if ($domain == $domain_fr)
-	{
 		echo '<meta name="description" content="Citations aléatoires postées sur '.$name_website.'. Citations de la vie quotidienne. Quotes ados."/>';
-	}
 	else
-	{
 		echo '<meta name="description" content="Random quotes released on '.$name_website.'. Because some quotes are simply true. Your everyday life moments."/>';
-	}
 	echo "\r\n";
+	
 	// Add a canonical URL
 	echo '<link rel="canonical" href="http://'.$domain.'/?p='.$page_random.'" />';
 }
@@ -83,7 +70,10 @@ elseif (isset($_GET['id_quote']))
 elseif (isset($_GET['letter']) OR preg_match('#members#', $_SERVER["SCRIPT_URI"])) 
 {
 	$lettre = mysql_real_escape_string($_GET['letter']);
-	if (empty($lettre)) { $lettre = "A"; }
+	
+	if (empty($lettre))
+		$lettre = "A";
+
 	$php_self = 'members-'.$lettre;
 
 	if ($domain == $domain_fr)
@@ -99,6 +89,7 @@ elseif (isset($_GET['letter']) OR preg_match('#members#', $_SERVER["SCRIPT_URI"]
 		echo '<meta name="description" content="Members beginning with '.$lettre.' on '.$name_website.'. '.$name_website.' : because some quotes are simply true. Your everyday life moments." />';
 	}
 	echo "\r\n";
+	
 	// Add a canonical URL
 	echo '<link rel="canonical" href="http://'.$domain.'/members" />';
 }
@@ -108,13 +99,9 @@ elseif ($php_self == 'contact')
 	echo "\r\n";
 
 	if ($domain == $domain_fr)
-	{
 		echo '<meta name="description" content="'.$name_website.' : contactez-nous par email pour toute question."/>';	
-	}
 	else
-	{
 		echo '<meta name="description" content="'.$name_website.' : contact us by email if you have any question."/>';
-	}
 	echo "\r\n";		
 }
 elseif ($php_self == 'signup')
@@ -123,28 +110,26 @@ elseif ($php_self == 'signup')
 	echo "\r\n";
 
 	if ($domain == $domain_fr)
-	{
 		echo '<meta name="description" content="'.$name_website.' : créez votre compte et accédez à tous les avantages qui vont avec : profils, citations favorites, ajout de commentaires..." />';
-	}
+
 	else
-	{
 		echo '<meta name="description" content="'.$name_website.' : create an account and be able to access all the advantages that come with it: profiles, favorite quotes, comments..." />';
-	}
+
 	echo "\r\n";
 }
 elseif ($php_self == 'apps')
 {
 	echo '<title>'.$name_website.' | '.$application.'</title>';
 	echo "\r\n";
+
 	if ($domain == $domain_fr)
-	{
 		echo '<meta name="description" content="'.$name_website.' : téléchargez notre application pour iOS et Android. Visitez notre version mobile depuis votre portable."/>';
-	}
+
 	else
-	{
 		echo '<meta name="description" content="'.$name_website.' : download our application for iOS and Android. Visit our mobile website right from your smartphone."/>';
-	}
+
 	echo "\r\n";
+	
 	// Add a canonical URL
 	echo '<link rel="canonical" href="http://'.$domain.'/apps" />';
 }
@@ -152,14 +137,13 @@ elseif ($php_self == 'advertise')
 {
 	echo '<title>'.$name_website.' | '.$advertise.'</title>';
 	echo "\r\n";
+
 	if ($domain == $domain_fr)
-	{
 		echo '<meta name="description" content="'.$name_website.' : avec plus de 1 700 000 followers sur Twitter, près de 50 000 fans sur Facebook et un site, Teen Quotes est une bonne opportunité pour proposer de la publicité."/>';
-	}
+
 	else
-	{
 		echo '<meta name="description" content="'.$name_website.' : with more than 1,700,000 followers on Twitter, nearly 50,000 fans on Facebook and a website, Teen Quotes is a really good opportunity for advertising."/>';
-	}
+
 	echo "\r\n";
 }
 elseif ($php_self == 'statistics')
@@ -167,13 +151,13 @@ elseif ($php_self == 'statistics')
 	echo '<title>'.$name_website.' | '.$statistics.'</title>';
 	echo "\r\n";
 	if ($domain == $domain_fr)
-	{
+
 		echo '<meta name="description" content="'.$name_website.' : statistiques. Quelques statistiques sur l\'utilisation du site : membres, citations, recherches..."/>';
-	}
+
 	else
-	{
+
 		echo '<meta name="description" content="'.$name_website.' : statistics. Some statistics about the use of the website : members, quotes, searchs..."/>';
-	}
+
 	echo "\r\n";
 }
 else 
@@ -192,13 +176,10 @@ else
 	}
 	echo "\r\n";
 }
-// Fin des différents cas de <title></title>
+
+// End <title></title>
 if ($domain == $domain_fr)
-{
 	echo '<meta name="keywords" content="Kotado, Quotes Ados, Citations Ados, Citations vie quotidienne, Citations adolescents, Teen Quotes, Pretty Web, Antoine Augusti, Twitter"/>';
-}
 else
-{
 	echo '<meta name="keywords" content="Teen Quotes, teenage quotes, teenager quotes, quotes for teenagers, teen qoutes, quotes, teen, citations, sentences, Augusti, Twitter, Facebook"/>';
-}
 echo "\r\n";
