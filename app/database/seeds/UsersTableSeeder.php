639bf1169529651123388bf26aa82689138e7375
Antoine Augusti <antoine@augusti.fr>
2014-04-20 13:50:54 +0200

    Adding an admin account when seeding the users table

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -1,6 +1,4 @@
 <?php
-
-// Composer: "fzaninotto/faker": "v1.3.0"
 use Faker\Factory as Faker;
 
 class UsersTableSeeder extends Seeder {
@@ -17,21 +15,44 @@ class UsersTableSeeder extends Seeder {
 		$this->command->info('Seeding Users table using Faker...');
 		foreach(range(1, 100) as $index)
 		{
-			User::create([
-				'login' => $faker->userName,
-				'password' => Hash::make("1234"),
-				'email' => $faker->email,
-				'ip' => $faker->ipv4,
-				'birthdate' => $faker->date('Y-m-d', 'now'),
-				'gender' => 'M',
-				'country' => $faker->country,
-				'city' => $faker->city,
-				'avatar' => 'icon50.png',
-				'about_me' => $faker->paragraph(3),
-				'hide_profile' => $faker->randomNumber(0, 1),
-				'notification_comment_quote' => $faker->randomNumber(0, 1),
-				'last_visit' => $faker->dateTimeThisYear(),
-			]);
+			// Random user
+			if ($index != 42) {
+				User::create([
+					'login' => $faker->userName,
+					'password' => Hash::make("1234"),
+					'email' => $faker->email,
+					'ip' => $faker->ipv4,
+					'birthdate' => $faker->date('Y-m-d', 'now'),
+					'gender' => 'M',
+					'country' => $faker->country,
+					'city' => $faker->city,
+					'avatar' => 'icon50.png',
+					'about_me' => $faker->paragraph(3),
+					'hide_profile' => $faker->randomNumber(0, 1),
+					'notification_comment_quote' => $faker->randomNumber(0, 1),
+					'last_visit' => $faker->dateTimeThisYear(),
+				]);
+			}
+			// Admin account
+			else {
+				User::create([
+					'login' => 'antoineaugusti',
+					'password' => Hash::make("1234"),
+					'email' => $faker->email,
+					'security_level' => 1,
+					'ip' => $faker->ipv4,
+					'birthdate' => $faker->date('Y-m-d', 'now'),
+					'gender' => 'M',
+					'country' => $faker->country,
+					'city' => $faker->city,
+					'avatar' => 'icon50.png',
+					'about_me' => $faker->paragraph(3),
+					'hide_profile' => 0,
+					'notification_comment_quote' => 1,
+					'last_visit' => $faker->dateTimeThisYear(),
+				]);
+				$this->command->info('Admin account : #42 - mulshan - 1234');
+			}
 		}
 
 	}
