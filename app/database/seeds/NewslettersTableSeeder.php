f02423948be39cd4168ad563ccd6f53f8f7f77f3
Antoine Augusti <antoine@augusti.fr>
2014-04-20 14:29:09 +0200

    Newsletter table seeder

---
+++ app/database/seeds/NewslettersTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+use Faker\Factory as Faker;
+
+class NewslettersTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$this->command->info('Deleting existing Newsletter table ...');
+		DB::statement('SET FOREIGN_KEY_CHECKS = 0'); 
+		Newsletter::truncate();
+		DB::statement('SET FOREIGN_KEY_CHECKS = 1'); 
+
+		$faker = Faker::create();
+
+		$this->command->info('Seeding Newsletter table using Faker...');
+		foreach(range(1, 80) as $index)
+		{
+			Newsletter::create([
+				'user_id' => $faker->randomNumber(1, 100),
+				'type' => $faker->randomElement(array('weekly', 'daily')),
+				'unsubscribe_code' => $faker->bothify('##??##???#'),
+			]);
+		}
+	}
+
+}
---
+++ app/models/Newsletter.php
@@ -0,0 +1,5 @@
+<?php
+
+class Newsletter extends Eloquent {
+	protected $fillable = [];
+}
