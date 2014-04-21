183303513195c2b2c7387db0856be7d61b4de0f7
Antoine Augusti <antoine@augusti.fr>
2014-04-21 12:52:20 +0200

    Current simple homepage

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,38 +1,19 @@
+<?php
+$i = 0;
+?>
 @include('layouts/header')
-<div class="navbar navbar-default navbar-fixed-top">
-	<div class="container">
-		<div class="navbar-header">
-			<a href="../" class="logo"></a>
-			<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
+<div class="container under-navbar">
+	@foreach ($quotes as $quote)
+		<div class="quote" style="background-color:{{$colors[$i]}};border-bottom-color:{{Quote::adjustBrightness($colors[$i], -20)}}">
+			#{{ $quote->id}}
+			{{ $quote->content}}
 		</div>
-		<div class="navbar-collapse collapse" id="navbar-main">
-			<ul class="nav navbar-nav">
-				<li>
-					<a href="/"><i class="fa fa-user"></i> {{ Lang::get('layout.login') }}</a>
-				</li>
-				<li>
-					<a href="/"><i class="fa fa-random"></i>{{ Lang::get('layout.randomQuotes') }}</a>
-				</li>
-				<li>
-					<a href="/"><i class="fa fa-comment"></i>{{ Lang::get('layout.addQuote') }}</a>
-				</li>
-				<li>
-					<a href="/"><i class="fa fa-mobile fa-lg"></i>{{ Lang::get('layout.apps') }}</a>
-				</li>
-			</ul>
+		<?php $i++ ?>	
+	@endforeach
+</div>
 
-			<ul class="nav navbar-nav navbar-right hidden-sm hidden-xs">
-				<li>
-					<a href="http://twitter.com/ohteenquotes" class="twitter-follow-button" data-show-count="true" data-lang="en">Follow @ohteenquotes</a>
-				</li>
-			</ul>
-
-		</div>
-	</div>
+<div class="text-center">
+	{{ $quotes->links() }}
 </div>
 
 @include('layouts/footer')
--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -10,6 +10,41 @@
 	{{ HTML::style('assets/css/screen.css'); }}
 	<!--[if lt IE 9]>
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
+		<![endif]-->
 </head>
 <body>
+	<div class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<div class="navbar-header">
+				<a href="../" class="logo"></a>
+				<button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div class="navbar-collapse collapse" id="navbar-main">
+				<ul class="nav navbar-nav">
+					<li>
+						<a href="/"><i class="fa fa-user"></i> {{ Lang::get('layout.login') }}</a>
+					</li>
+					<li>
+						<a href="/"><i class="fa fa-random"></i>{{ Lang::get('layout.randomQuotes') }}</a>
+					</li>
+					<li>
+						<a href="/"><i class="fa fa-comment"></i>{{ Lang::get('layout.addQuote') }}</a>
+					</li>
+					<li>
+						<a href="/"><i class="fa fa-mobile fa-lg"></i>{{ Lang::get('layout.apps') }}</a>
+					</li>
+				</ul>
+
+				<ul class="nav navbar-nav navbar-right hidden-sm hidden-xs">
+					<li>
+						<a href="http://twitter.com/ohteenquotes" class="twitter-follow-button" data-show-count="true" data-lang="en">Follow @ohteenquotes</a>
+					</li>
+				</ul>
+
+			</div>
+		</div>
+	</div><!-- END NAVBAR -->
