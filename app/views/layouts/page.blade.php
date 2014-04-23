84b3657bfe79945e38adbe4137e0c93d8e31fe6b
Antoine Augusti <antoine@augusti.fr>
2014-04-23 16:57:49 +0200

    Add a smiley inside an alert when everything went fine

--- app/views/layouts/page.blade.php
+++ app/views/layouts/page.blade.php
@@ -4,7 +4,7 @@
 	@if(Session::has('success'))
 		<div class="animated flipInX alert alert-success alert-dismissable">
 			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-			{{ Session::get('success') }}
+			<i class="fa fa-smile-o"></i> {{ Session::get('success') }}
 		</div>
 	@endif
 
