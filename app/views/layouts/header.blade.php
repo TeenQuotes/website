3152182d6629c158a98122dc8358d99be93b7b2b
Antoine Augusti <antoine@augusti.fr>
2014-04-24 18:24:52 +0200

    Fix favicon's link

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -8,7 +8,7 @@
 	{{ HTML::style('//netdna.bootstrapcdn.com/bootswatch/3.1.1/cosmo/bootstrap.min.css'); }}
 	{{ HTML::style('//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css'); }}
 	{{ HTML::style('//cdnjs.cloudflare.com/ajax/libs/animate.css/3.1.0/animate.min.css'); }}
-	{{ HTML::style('assets/css/screen.css'); }}
+	{{ HTML::style('/assets/css/screen.css'); }}
 	<link rel="shortcut icon" href="assets/images/favicon.png"/>
 
 	<!-- TWITTER CARD FOR SINGLE QUOTE -->
