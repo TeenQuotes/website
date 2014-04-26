b1219a81a1e558b81fee12ee0739dadea6b3b28b
Antoine Augusti <antoine@augusti.fr>
2014-04-26 12:26:05 +0200

    Fixing call to not allowed variable in translation

--- app/lang/en/auth.php
+++ app/lang/en/auth.php
@@ -29,5 +29,5 @@ return array(
 	'iveLostMyPassword'       => "I've lost my password!",
 	'lostCheerUp'             => "Don't worry, we are here to help! We deal with issues like this everyday. Our robots will take care of you, just your enter email address bellow and check your inbox!",
 	'heyEmail'                => 'Hey!',
-	'askedResetPasswordEmail' => "You've just asked to reset your password on ".Lang::get('auth.nameWebsite')." and we are here to help!<br/><br/>Click the link, complete the form and you will be out of trouble in a minute:",
+	'askedResetPasswordEmail' => "You've just asked to reset your password on Teen Quotes and we are here to help!<br/><br/>Click the link, complete the form and you will be out of trouble in a minute:",
 );
