0219cfb839a1c0a1dc8e1281414d3e4515cc32dd
Antoine Augusti <antoine@augusti.fr>
2014-04-26 19:27:32 +0200

    Base for the users.show view

---
+++ app/views/users/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts/page')
+<?php
+$i = 0;
+?>
+@section('content')
+	@foreach ($quotes as $quote)
+		@include('quotes.singleQuote', compact($quote))
+	<?php $i++ ?>	
+	@endforeach
+
+	<div class="text-center">
+		{{ $paginator->links() }}
+	</div>
+@stop
