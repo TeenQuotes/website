35b06eadec6a1d97d0dae52f3b63c1b9cc9c567a
Antoine Augusti <antoine@augusti.fr>
2014-04-27 12:05:29 +0200

    Using lang variables in emails views

--- app/lang/en/email.php
+++ app/lang/en/email.php
@@ -1,5 +1,8 @@
 <?php
 return array(
 	'robotFooter' => "Please do not reply to this message. This email was sent by our beloved robot ; unfortunately, our robot can't read (but we are trying hard). If you wish to contact us, you can do so via the form at teen-quotes.com/contact or directly at support@teen-quotes.com",
-	
+	'URLLogo'     => 'http://dev.teen-quotes.com/assets/images/logo.png',
+	'iconWebsite' => 'http://teen-quotes.com/mail/site_web.png',
+	'iconApp'     => 'http://teen-quotes.com/mail/app.png',
+	'iconTwitter' => 'http://teen-quotes.com/mail/twitter.png',
 );
