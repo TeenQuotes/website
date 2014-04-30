d45b421c8e986291087c934e3a66a05ee3eb6240
Antoine Augusti <antoine@augusti.fr>
2014-04-30 11:38:37 +0200

    Updating page titles to use $nameWebsite in it

--- app/lang/en/quotes.php
+++ app/lang/en/quotes.php
@@ -1,8 +1,8 @@
 <?php
 return array (
 	'homePageTitle'           => 'Teen Quotes | Because some quotes are simply true',
-	'homePageDescription'     => 'Teen Quotes : because our lives are filled full of beautiful sentences, and because some quotes are simply true. Your every day life moments.',
-	'randomPageTitle'         => 'Random quotes | Teen Quotes',
+	'homePageDescription'     => 'Teen Quotes: because our lives are full of beautiful sentences. Your everyday life moments. Because some quotes are simply true.',
+	'randomPageTitle'         => 'Random quotes | '.Lang::get('layout.nameWebsite'),
 	'randomPageDescription'   => 'Random quotes released on Teen Quotes. Because some quotes are simply true. Your everyday life moments.',
 	'yourQuote'               => 'Your quote',
 	'addYourQuote'            => 'Add your quote',
@@ -11,7 +11,7 @@ return array (
 	'fewRules'                => 'A few rules',
 	'rulesAddQuote'           => '<li>Your quote must be written in proper English</li><li>Your quote should not already be on Teen Quotes</li><li>Your quote should not be too sad</li>',
 	'noticeByEmail'           => 'Your quote will then be approved or rejected by our team, you will be informed by email.',
-	'addquotePageTitle'       => 'Add a quote | Teen Quotes',
+	'addquotePageTitle'       => 'Add a quote | '.Lang::get('layout.nameWebsite'),
 	'addquotePageDescription' => 'Speak your mind and share your thoughts with the world. Add a quote right now!',
 	'quoteAddedSuccessfull'   => 'Your quote has been submitted :login! You\'ll get an email soon.',
 	'commentComments'         => '{1} :nb comment| [2,Inf] :nb comments',
--- app/lang/en/users.php
+++ app/lang/en/users.php
@@ -2,6 +2,6 @@
 return array(
 	'favoriteQuotes'         => 'Favorite quotes',
 	'publishedQuotes'        => 'Published quotes',
-	'profilePageTitle'       => ":login's profile | Teen Quotes",
+	'profilePageTitle'       => ":login's profile | ".Lang::get('layout.nameWebsite'),
 	'profilePageDescription' => 'Visit the profile of :login on Teen Quotes and learn some cool facts!',
 );
