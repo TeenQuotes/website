6ee2ed671ea231df541040b304f656afa8acfd6a
Antoine Augusti <antoine@augusti.fr>
2014-05-01 16:46:28 +0200

    Changing effect for warning banner

--- app/views/layouts/page.blade.php
+++ app/views/layouts/page.blade.php
@@ -9,7 +9,7 @@
 	@endif
 
 	@if(Session::has('warning'))
-		<div class="animated flipInX alert alert-warning alert-dismissable">
+		<div class="animated lightSpeedIn alert alert-warning alert-dismissable">
 			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 			<i class="fa fa-meh-o"></i> {{ Session::get('warning') }}
 		</div>
