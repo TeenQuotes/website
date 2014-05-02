076e42e4a9fc5fd330df28dbec50939c800500a3
Antoine Augusti <antoine@augusti.fr>
2014-05-02 11:59:47 +0200

    Scopes for newsletters

--- app/models/Newsletter.php
+++ app/models/Newsletter.php
@@ -7,4 +7,17 @@ class Newsletter extends Eloquent {
 	{
 		return $this->belongsTo('User');
 	}
+
+	public function scopeType($query, $type)
+	{
+		if (!in_array($type, array('weekly', 'daily')))
+    		throw new InvalidArgumentException("Newsletter's type only accepts weekly or daily. ".$type." was given.");
+
+    	return $query->whereType($type);
+	}
+
+	public function scopeForUser($query, User $user)
+	{
+		return $query->where('user_id', '=', $user->id);
+	}
 }
