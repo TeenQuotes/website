cf8777cfe3d6b01777b8bc76ab8fc1ff304223a3
Antoine Augusti <antoine@augusti.fr>
2014-05-08 17:51:50 +0200

    Fix welcome => hi when sending the birthday email

--- app/views/emails/birthday.blade.php
+++ app/views/emails/birthday.blade.php
@@ -5,7 +5,7 @@ $carbon = Carbon::createFromFormat('Y-m-d', $user['birthdate']);
 
 @section('content')
 	<img src="{{ Lang::get('email.imageBirthday') }}" id="img-birthday" />
-	{{ Lang::get('auth.welcomeEmailWithUsername', array('login' => $user['login'])) }}
+	{{ Lang::get('email.hiWithLogin', array('login' => $user['login'])) }}
 	<br/><br/>
 	{{ Lang::get('email.birthdayContent', ['login' => $user['login'], 'age' => $carbon->age]) }}
 @stop
