85209c80212d8e44322ee2db446ae08e8670a6e8
Antoine Augusti <antoine@augusti.fr>
2014-05-08 10:55:11 +0200

    Fix tense for a phrase

--- app/lang/en/email.php
+++ app/lang/en/email.php
@@ -1,7 +1,7 @@
 <?php
 return array(
 	'robotFooter'         => "Please do not reply to this message. This email was sent by our beloved robot ; unfortunately, our robot can't read (but we are trying hard). If you wish to contact us, you can do so via the form at teen-quotes.com/contact or directly at support@teen-quotes.com",
-	'manageEmailSettings' => 'You receive this email because your settings are set this way. You can manage your email settings when editing your profile. To edit your profile, please follow this link :url',
+	'manageEmailSettings' => 'You received this email because your settings are set this way. You can manage your email settings when editing your profile. To edit your profile, please follow this link :url',
 	'hiWithLogin'         => 'Hi :login!,',
 	// TODO: change URL
 	'URLLogo'             => 'http://dev.teen-quotes.com/assets/images/logo.png',
