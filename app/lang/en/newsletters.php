773483c4a8422833e81193e878bbd6f317d40c93
Antoine Augusti <antoine@augusti.fr>
2014-05-08 09:53:03 +0200

    Fix typo

--- app/lang/en/newsletters.php
+++ app/lang/en/newsletters.php
@@ -1,6 +1,6 @@
 <?php
 return array(
 	'weeklySubjectEmail' => 'The weekly newsletter',
-	'beenWaitingForLong' => "You've been waiting for a long time, but here it is! The weekly newsletter is out and it is in your inbox!<br/><br/>Here a bunch of cool quotes published on ".Lang::get('layout.nameWebsite').":",
+	'beenWaitingForLong' => "You've been waiting for a long time, but here it is! The weekly newsletter is out and it is in your inbox!<br/><br/>Here is a bunch of cool quotes published on ".Lang::get('layout.nameWebsite').":",
 	'callToVisitWebsite' => "And remember: if you want to read more awesome quotes there is only a place to do that, and it's called ".Lang::get('layout.nameWebsite')."!",
 );
