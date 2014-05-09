98bc87dd5e15898e4147fcad141e6a254614e5ff
Antoine Augusti <antoine@augusti.fr>
2014-05-09 09:32:50 +0200

    Add a campain to track when editing profile after receiving the welcome email

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -1,7 +1,11 @@
 @extends('emails/default')
+<?php
+$linkEditProfile = URL::route('users.edit', array($login));
+$linkCampaignText = TextTools::linkCampaign($linkEditProfile, 'callToEditProfile', 'email', 'welcome', 'linkBodyEmail');
+?>
 
 @section('content')
 	{{ Lang::get('auth.welcomeEmailWithUsername', array('login' => $login)) }}
 	<br/><br/>
-	{{ Lang::get('auth.bodyWelcomeEmail', array('login' => $login, 'linkEditProfile' => URL::route('users.edit', array($login)))) }}
+	{{ Lang::get('auth.bodyWelcomeEmail', array('login' => $login, 'linkEditProfile' => $linkCampaignText, 'url' => $linkEditProfile)) }}
 @stop
