67cc8a12194ed4c553fadba9648458d760a58ee0
Antoine Augusti <antoine@augusti.fr>
2014-05-09 19:32:43 +0200

    Add scopes hidden / notHidden for the User Model

--- app/models/User.php
+++ app/models/User.php
@@ -168,6 +168,16 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
     	return $query->where(DB::raw("DATE_FORMAT(birthdate,'%m-%d')"), '=', DB::raw("DATE_FORMAT(NOW(),'%m-%d')"));
     }
 
+    public function scopeNotHidden($query)
+    {
+    	return $query->where('hide_profile', '=', 0);
+    }
+
+    public function scopeHidden($query)
+    {
+    	return $query->where('hide_profile', '=', 1);
+    }
+
     public function scopePartialLogin($query, $login)
     {
     	return $query->whereRaw('login LIKE ?', ["%$login%"])->orderBy('login', 'ASC');
