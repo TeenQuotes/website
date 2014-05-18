c27c2db5fc4dee7f3e8d75dceec57dc334bf2353
Antoine Augusti <antoine@augusti.fr>
2014-05-18 19:09:12 +0200

    User profile : do not display controls if the user is not viewing his profile

--- app/views/users/profile/controls.blade.php
+++ app/views/users/profile/controls.blade.php
@@ -1,7 +1,7 @@
-<div class="btn-group">
-	@if (Auth::check() AND Auth::user()->login == $user->login)
-		<a href="{{URL::route('users.edit', $user->login)}}"><button type="button" class="btn btn-warning"><i class="fa fa-edit"></i> {{Lang::get('users.editProfileTitle')}}</button></a>
-		<a href="{{URL::route('logout')}}"><button type="button" class="btn btn-danger"><i class="fa fa-sign-out"></i> {{Lang::get('layout.logout')}}</button></a>
-	@endif
-</div>
-<div class="clearfix"></div>
+@if (Auth::check() AND Auth::user()->login == $user->login)
+	<div class="btn-group">
+			<a href="{{URL::route('users.edit', $user->login)}}"><button type="button" class="btn btn-warning"><i class="fa fa-edit"></i> {{Lang::get('users.editProfileTitle')}}</button></a>
+			<a href="{{URL::route('logout')}}"><button type="button" class="btn btn-danger"><i class="fa fa-sign-out"></i> {{Lang::get('layout.logout')}}</button></a>
+	</div>
+	<div class="clearfix"></div>
+@endif
