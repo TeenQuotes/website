819eab19492b6009d31b9c8e11421a8a630c0028
Antoine Augusti <antoine@augusti.fr>
2014-05-20 19:54:40 +0200

    Generate more fake comments

--- app/database/seeds/CommentsTableSeeder.php
+++ app/database/seeds/CommentsTableSeeder.php
@@ -15,11 +15,11 @@ class CommentsTableSeeder extends Seeder {
 
 		$this->command->info('Seeding Comments table using Faker...');
 		$date = Carbon::createFromDate(2011, 12, 1);
-		foreach(range(1, 400) as $index)
+		foreach(range(1, 1500) as $index)
 		{
 			Comment::create([
 				'content'    => $faker->paragraph(3),
-				'quote_id'   => $faker->randomNumber(150, 250),
+				'quote_id'   => $faker->randomNumber(150, 750),
 				'user_id'    => $faker->randomNumber(1, 100),
 				'created_at' => $date,
 			]);
