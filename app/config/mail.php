8f68543098f0592032b3324f7d8d5a6816ea4a38
Antoine Augusti <antoine@augusti.fr>
2014-06-04 19:45:21 +0200

    Rolling back the old email setup

--- app/config/mail.php
+++ app/config/mail.php
@@ -16,7 +16,7 @@ return array(
 	|
 	*/
 
-	'driver' => 'mailgun',
+	'driver' => 'smtp',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'hello@dev.teen-quotes.com', 'name' => 'Teen Quotes'),
+	'from' => array('address' => 'antoine.augusti@quantic-telecom.net', 'name' => 'Teen Quotes'),
 
 	/*
 	|--------------------------------------------------------------------------
