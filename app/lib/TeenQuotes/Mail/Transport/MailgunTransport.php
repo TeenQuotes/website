922d72704a6350402ea11954873617a15832209e
Antoine Augusti <antoine@augusti.fr>
2014-06-06 21:15:02 +0200

    Fix indentation

--- app/lib/TeenQuotes/Mail/Transport/MailgunTransport.php
+++ app/lib/TeenQuotes/Mail/Transport/MailgunTransport.php
@@ -17,30 +17,31 @@ class MailgunTransport extends \Illuminate\Mail\Transport\MailgunTransport {
 		$client = $this->getHttpClient();
 
 		// Inline CSS here
-        $converter = new CssToInlineStyles();
+		$converter = new CssToInlineStyles();
 		$converter->setEncoding($message->getCharset());
-        $converter->setUseInlineStylesBlock();
-        $converter->setCleanup();
+		$converter->setUseInlineStylesBlock();
+		$converter->setCleanup();
 
-        if ($message->getContentType() === 'text/html' ||
-            ($message->getContentType() === 'multipart/alternative' && $message->getBody())
-        ) {
-            $converter->setHTML($message->getBody());
-            $message->setBody($converter->convert());
-        }
+		if ($message->getContentType() === 'text/html' ||
+			($message->getContentType() === 'multipart/alternative' && $message->getBody())
+		) {
+			$converter->setHTML($message->getBody());
+			$message->setBody($converter->convert());
+		}
 
-        foreach ($message->getChildren() as $part) {
-            if (strpos($part->getContentType(), 'text/html') === 0) {
-                $converter->setHTML($part->getBody());
-                $part->setBody($converter->convert());
-            }
-        }
+		foreach ($message->getChildren() as $part) {
+			if (strpos($part->getContentType(), 'text/html') === 0) {
+				$converter->setHTML($part->getBody());
+				$part->setBody($converter->convert());
+			}
+		}
 
+		// Call the API
 		$client->post($this->url, ['auth' => ['api', $this->key],
 			'body' => [
-	    		'to' => $this->getTo($message),
-	    		'message' => new PostFile('message', (string) $message),
-	    	],
-    	]);
+				'to' => $this->getTo($message),
+				'message' => new PostFile('message', (string) $message),
+			],
+		]);
 	}
 }
