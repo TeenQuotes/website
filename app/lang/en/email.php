522264a1c5f8d969d0aca1926e760c579aeae182
Antoine Augusti <antoine@augusti.fr>
2014-06-06 21:14:53 +0200

    Correct typo and fix URLs for images in emails

--- app/lang/en/email.php
+++ app/lang/en/email.php
@@ -6,21 +6,14 @@ return array(
 	'happyBirthdaySubjectEmail'  => 'Happy birthday!',
 	'birthdayContent'            => "Today is a special day for you :login! :age years old, how amazing it is? You will have to celebrate this day with your family and your friends!<br/><br/> All the team want to wish you a happy birthday! We hope that you will have a great day :)<br/><br/>See you soon on ".Lang::get('layout.nameWebsite')." and have a nice day!",
 	'eventChristmasSubjectEmail' => "Merry Christmas!",
-	'christmasBody'              => "The entire team of ".Lang::get('layout.nameWebsite')." wish you a Merry Christmas and hope you enjoy these few days of family vacation. We hope you've been nice this year and that Santa will give you great gifts!<br/><br/>We're looking forward to see you soon on ".Lang::get('layout.nameWebsite')."!",
+	'christmasBody'              => "The entire team of ".Lang::get('layout.nameWebsite')." wishes you a Merry Christmas and hope you enjoy these few days of family vacation. We hope you've been nice this year and that Santa will give you great gifts!<br/><br/>We're looking forward to see you soon on ".Lang::get('layout.nameWebsite')."!",
 	'eventNewyearSubjectEmail'   => "Happy New Year!",
 	'newyearBody'                => "New is the year, new are the hopes and the aspirations. New is the resolution, new are the spirits and forever our warm wishes are for you. Have a promising and fulfilling new year!<br/><br/>The entire team of ".Lang::get('layout.nameWebsite')." wish you a Happy New Year! We hope this year will awesome for you!<br/><br/>We're looking forward to see you soon on ".Lang::get('layout.nameWebsite')."!",
-	// TODO: change URL
-	'URLLogo'                    => 'http://dev.teen-quotes.com/assets/images/logo.png',
-	// TODO: change URL
-	'iconWebsite'                => 'http://dev.teen-quotes.com/assets/images/emails/website.png',
-	// TODO: change URL
-	'iconApp'                    => 'http://dev.teen-quotes.com/assets/images/emails/app.png',
-	// TODO: change URL
-	'iconTwitter'                => 'http://dev.teen-quotes.com/assets/images/emails/twitter.png',
-	// TODO: change URL
-	'imageBirthday'              => 'http://dev.teen-quotes.com/assets/images/emails/birthday.png',
-	// TODO: change URL
-	'imageChristmas'             => 'http://dev.teen-quotes.com/assets/images/emails/christmas.png',
-	// TODO: change URL
-	'imageNewyear'               => 'http://dev.teen-quotes.com/assets/images/emails/newyear.png',
+	'URLLogo'                    => Config::get('app.domain').'/assets/images/logo.png',
+	'iconWebsite'                => Config::get('app.domain').'/assets/images/emails/website.png',
+	'iconApp'                    => Config::get('app.domain').'/assets/images/emails/app.png',
+	'iconTwitter'                => Config::get('app.domain').'/assets/images/emails/twitter.png',
+	'imageBirthday'              => Config::get('app.domain').'/assets/images/emails/birthday.png',
+	'imageChristmas'             => Config::get('app.domain').'/assets/images/emails/christmas.png',
+	'imageNewyear'               => Config::get('app.domain').'/assets/images/emails/newyear.png',
 );
