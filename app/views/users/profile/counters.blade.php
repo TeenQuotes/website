c6c5f611d48c572eabd05831955e6cc6aed0ce6d
Antoine Augusti <antoine@augusti.fr>
2014-06-17 11:07:13 +0200

    Add a ID to a profile's counter when viewing self profile

--- app/views/users/profile/counters.blade.php
+++ app/views/users/profile/counters.blade.php
@@ -1,8 +1,9 @@
-<?php $openedLink = false; ?>
 <div class="row stats-counter">
 	@foreach (['quotesPublishedCount', 'favCount', 'commentsCount', 'addedFavCount'] as $element)
+		<?php $openedLink = false; ?>
 		@if ($$element > 0)
 			<div class="col-xs-6 col-md-3 text-center">
+				<!-- LINKS  -->
 				@if ($type != 'favorites' AND $element == 'favCount')
 					<a href="{{ URL::route('users.show', [$user->login, 'favorites']) }}">
 					<?php $openedLink = true; ?>
@@ -19,13 +20,20 @@
 				@endif
 
 				<span class="description">{{ Lang::choice('users.'.$element.'Text', $$element) }}</span>
-				<span class="counter">{{ $$element }}</span>
+				
+				<!-- COUNTER  -->
+				<!-- Add an ID to the counter to increase / decrease on the user's profile -->
+				@if (Auth::check() AND Auth::user()->login == $user->login)
+					<span class="counter" id="{{ Str::snake($element, '-') }}">{{ $$element }}</span>
+				@else
+					<span class="counter">{{ $$element }}</span>
+				@endif
 
 				@if ($openedLink)
 					</a>
 				@endif
 
-				<!-- Active or not -->
+				<!-- ACTIVE OR NOT -->
 				@if (($type == 'comments' AND $element == 'commentsCount') OR ($type == 'published' AND $element == 'quotesPublishedCount') OR ($type == 'favorites' AND $element == 'favCount'))
 					<div class="active"></div>
 				@else
