1e18ab0403b1dabc2d8e11e92635d5b3b4c8c4a7
Antoine Augusti <antoine@augusti.fr>
2014-06-18 17:33:01 +0200

    Correct link in email's footer

--- app/views/emails/layout/footer.blade.php
+++ app/views/emails/layout/footer.blade.php
@@ -6,7 +6,7 @@
 				<img src="{{ Lang::get('email.iconWebsite') }}" class="icon"/>{{ Lang::get('email.footerWebsite') }} <a href="http://{{ Config::get('app.domain') }}" title="Website">{{ Config::get('app.domain') }}</a><br/>
 				<img src="{{ Lang::get('email.iconApp') }}" class="icon"/>{{ Lang::get('email.footerApplication') }}: <a href="{{ Lang::get('layout.downloadLinkiOS') }}" title="iPhone">{{ Config::get('app.domain') }}/apps</a><br/>
 				<img src="{{ Lang::get('email.iconTwitter') }}" class="icon"/>{{ Lang::get('email.footerTwitter') }} <a href="http://twitter.com/{{str_replace('@', '', Lang::get('layout.twitterUsername'))}}" title="Twitter">{{ Lang::get('layout.twitterUsername') }}</a><br/>
-				<img src="{{ Lang::get('email.iconStories') }}" class="icon"/>{{ Lang::get('email.footerTwitter') }} <a href="{{URL::route('stories')}}" title="Stories">{{ Config::get('app.domainStories') }}</a><br/>
+				<img src="{{ Lang::get('email.iconStories') }}" class="icon"/>{{ Lang::get('email.footerStories') }} <a href="{{URL::route('stories')}}" title="Stories">{{ Config::get('app.domainStories') }}</a><br/>
 			</div>
 		</div><!-- BODY CONTENT -->
 	</div><!-- CONTAINER -->
