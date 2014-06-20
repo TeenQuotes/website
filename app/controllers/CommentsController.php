ef3daf661b7d39edc0fc62f3b9aa8ac14b224d12
Antoine Augusti <antoine@augusti.fr>
2014-06-20 11:32:29 +0200

    Use the API to post a new comment

--- app/controllers/CommentsController.php
+++ app/controllers/CommentsController.php
@@ -40,33 +40,14 @@ class CommentsController extends \BaseController {
 		];
 
 		$validator = Validator::make($data, Comment::$rulesAdd);
-		$quote = Quote::where('id', '=', $data['quote_id'])->with('user')->first();
 
 		// Check if the form validates with success.
-		if ($validator->passes() AND !is_null($quote) AND $quote->isPublished()) {
+		if ($validator->passes()) {
 
-			// Store the comment
-			$comment = new Comment;
-			$comment->content  = $data['content'];
-			$comment->quote_id = $data['quote_id'];
-			$comment->user_id  = Auth::user()->id;
-			$comment->save();
-
-			// Send an email to the author of the quote if he wants it
-			if ($quote->user->wantsEmailComment()) {
-				$emailData = array();
-				$emailData['quote']   = $quote->toArray();
-				$emailData['comment'] = $comment->toArray();
-
-				Mail::send('emails.comments.posted', $emailData, function($m) use($quote)
-				{
-					$m->to($quote->user->email, $quote->user->login)->subject(Lang::get('comments.commentAddedSubjectEmail', ['id' => $quote->id]));
-				});
-			}
-
-			// If we have the number of comments in cache, increment it
-			if (Cache::has(Quote::$cacheNameNbComments.$data['quote_id']))
-				Cache::increment(Quote::$cacheNameNbComments.$data['quote_id']);
+			// Call the API - skip the API validator
+			$response = App::make('CommentsAPIv1Controller')->store($data['quote_id'], false);
+			if ($response->getStatusCode() != 200)
+				return Redirect::action('QuotesController@show',  ['id' => $data['quote_id']])->withErrors($validator)->withInput(Input::all());
 
 			return Redirect::action('QuotesController@show',  ['id' => $data['quote_id']])->with('success', Lang::get('comments.commentAddedSuccessfull'));
 		}
