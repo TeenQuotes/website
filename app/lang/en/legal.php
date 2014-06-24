c602dbbef08ad87be76edca5836eefa9ae0aa326
Antoine Augusti <antoine@augusti.fr>
2014-06-24 16:04:16 +0200

    Content for legal pages

---
+++ app/lang/en/legal.php
@@ -0,0 +1,85 @@
+<?php
+return array(
+	'termsOfUseTitle' => 'Terms of use',
+	'termsOfUseContent' => '
+	<p>
+		The use of submissions found in the server '.Config::get('app.domain').' is authorized under condition of non-alteration of its contents and the restriction of its use to personal use only and for the purpose of information. Its use for commercial activities is strictly prohibited. Any other use than the above herein mentioned is strictly prohibited by law.<br/><br/>
+		Anyone who does not respect the applicable provisions is guilty of the delict of piracy and is object of the penalties stipulated by law. The information provided does not include the design or the pagination of '.Config::get('app.domain').' or of any other site managed, controlled or owned by '.Lang::get('layout.nameWebsite').'.<br/><br/>
+		The elements contained on the websites '.Lang::get('layout.nameWebsite').' are protected by Industrial design rights, the rights of author, trademark law and the applicable regulations to unfair competition and they can not be copied or reproduced in whole or in part without the prior written permission from '.Lang::get('layout.nameWebsite').'. No logo, graphic, sound or image from the website '.Lang::get('layout.nameWebsite').' can be copied, reproduced or distributed. '.Lang::get('layout.nameWebsite').' does not state any particular appropriate use for the content of submissions published by this site nor for the graphics in the site presented.<br/><br/>
+		Every article and every graphic is supplied AS IS without any guarantee whatsoever. '.Lang::get('layout.nameWebsite').' excludes any kind of implicit guarantee regarding the information above mentioned, particularly any implicit guarantee of quality, of adequacy to a particular use, of property and of absence of piracy.<br/><br/>
+		In no event and under no circumstance will '.Lang::get('layout.nameWebsite').' or its directors be responsible or liable to you or any third person in any manner for any indirect, consequential, exemplary, incidental or accessory damage nor for any damages resulting from the inappropriate interpretation of an article, lack of information and defamation or any other damage related to the paging of the information redistributed by this service.<br/><br/>
+		The submissions redistributed and the graphics used on the website '.Config::get('app.domain').' may contain inaccuracies and misspells. Changes are periodically added to the information source referred to by the search engine.<br/><br/>
+		Finally '.Lang::get('layout.nameWebsite').' reserves the right, at its sole discretion, to change and modify the services provided by this website. Provided that you are eligible for use of the site, you are granted a limited license to access and use the site and the site content to which you have properly gained access solely for your personal, non-commercial use in a non illicit manner and in no manner to prejudice '.Lang::get('layout.nameWebsite').' and users that contribute by the posting of comments and submissions.<br/><br/>
+		Without limiting the general grounds of this article you may not use the website '.Config::get('app.domain').' in a manner to prejudice, put out of business, condemn and compromise '.Lang::get('layout.nameWebsite').'.<br/><br/>
+		Regarding the comments and other submissions by any member, the author of the before mentioned submissions acknowledges full agreement to '.Lang::get('layout.nameWebsite').' to (1) use, copy, distribute, transmit, publicly display, publicly perform, reproduce, edit, modify, translate and reformat his or her contribution within the framework of the website '.Config::get('app.domain').' and any of its partners, (2) to redistribute its rights in the full extent permitted by the applicable regulations.<br/><br/>
+		The contributions posted on '.Config::get('app.domain').' are not financially compensated. '.Config::get('app.domain').' reserves the right to remove comments and submissions at any time without prior notice. By submitting or posting to any part of the site, you automatically grant, and you represent and warrant that you have the right to grant to '.Lang::get('layout.nameWebsite').' the authorizations stipulated in the present article. '.Lang::get('layout.nameWebsite').' reserves the right to change, modify, add, or delete portions of the terms of use and caution sentences of its websites at any time without further notice. It is your responsibility to regularly check the site to determine if there have been changes to these terms of use and to review such changes. Your continued use of the service or the site '.Config::get('app.domain').' after any such changes constitutes your acceptance of the new terms of use.
+	</p>',
+	'privacyPolicyTitle' => 'Privacy Policy',
+	'privacyPolicyContent' => "
+	<p>
+		This Privacy Policy discloses the privacy practices of the ".Config::get('app.domain')." website (the Site). Specifically, it outlines the types of information that we gather about you while you are using the Site, and the ways in which we use and share this information.<br/><br/>
+		We will not use the information that we collect while you are using the Site, nor will we authorize third parties to use such information, to mail or email promotional offers directly to you unless you have specifically informed us that you would like to receive such promotional offers.<br/><br/>
+		Please read this Privacy Policy carefully. By visiting and using the Site, you agree that your use of our Site, and any dispute over our online privacy practices, is governed by this Privacy Policy and of Terms of use. Because the Web is an evolving medium, we may need to change our Privacy Policy at some point in the future, in which case we'll post the revised Privacy Policy on this website and update the Last Updated date to reflect the date of the changes. By continuing to use the Site after we post any such changes, you accept the Privacy Policy as modified.<br/><br/>
+	</p>
+
+	<h2>What Information is Collected About Visitors?</h2>
+	<p>
+		The information that we collect about you while you are using the Site falls into two general categories, as described below. The information we collect may be collected directly by us, or it may be collected by a third-party website hosting provider, or another third-party service provider, on our behalf.
+	</p>
+
+	<h3>1. Information You Provide to Us</h3>
+	<p>
+		We may collect and store information, including personally-identifiable information (such as your name, postal address or email address), or other information, that you voluntarily supply to us either while on the Site or in responding via email in connection with a feature provided on the Site. Some examples of this type of information include information that you electronically submit when you contact us with questions, information that you post on blogs, discussion forums or other community posting and social networking areas on our Site, information that you electronically submit when you complete an online registration form to access and use certain features of our Site, and information contained in search requests or questions that you submit to the Site. If you decline to supply or provide us with certain information while using the Site, you may not be able to use or participate in some or all of the features offered through the Site.
+	</p>
+
+	<h3>2. Information that is Automatically Collected</h3>
+	<p>
+		We also collect and store non-personally identifiable information that is generated automatically as you navigate through the Site. For example, we may collect information about your computer's connection to the Internet, which allows us, among other things, to improve the delivery of our web pages to you and to measure traffic on the Site. We also may use a standard feature found in browser software called a cookie to enhance your experience with the Site. Cookies are small files that your web browser places on your hard drive for record-keeping purposes. By showing how and when visitors use the Site, cookies help us deliver advertisements, identify how many unique users visit us, and track user trends and patterns. They also prevent you from having to re-enter your preferences on certain areas of the Site where you have entered preference information before. This Site also may use web beacons (single-pixel graphic files also known as transparent GIFs) to access cookies and to count users who visit the Site or open our HTML-formatted email messages.<br/><br/>
+		If you want to limit the non-personally identifiable information that is automatically collected while you use our Site, most Web browsers allow you to disable cookies. To disable cookies using Internet Explorer, select “Internet Options” under the Tools menu and click on “Privacy.” Select “High” or “Block All Cookies.” To disable cookies using Mozilla Firefox, select “Options” under the “Tools” menu. If you choose to disable cookies, you may not be able to use or participate in some or all of the features offered through the Site.
+	</p>
+	
+	<h2>How is this Information Used?</h2>
+	<p>
+		We use the information we collect from you while you are using the Site in a variety of ways, including, for example, to process your registration request, provide you with services and communications that you have requested, customize features and advertising that appear on the Site, deliver our Site content to you, measure Site traffic, measure user interests and traffic patterns, and improve the Site and the services and features offered via the Site.<br/><br/>
+		We will not use the information that we collect while you are using the Site, nor will we authorize third parties to use such information, to mail or email promotional offers directly to you unless you have specifically informed us that you would like to receive such promotional offers.<br/><br/>
+		Otherwise, we may share information we collect while you are using the Site with our affiliates and with trusted third-party service providers who assist us in providing services. If you do not wish your personally identifiable information to be used or shared for these purposes, send a message to contact@".Config::get('app.domain').".<br/><br/>
+		For information about use of your information by us and by third-party advertising service providers for advertising and by , see the section “Data Collected in Connection with Ad Serving and Targeting.”<br/><br/>
+		Our Site also includes links to other websites and provides access to products and services offered by third parties, whose privacy policies we do not control. When you access another website or purchase products or services from a third party Site, use of any information you provide is governed by the privacy policy of the operator of the site you are visiting or the provider of such products or services.<br/><br/>
+		We also make some content, products and services available through our Site through cooperative relationships with third party providers. We may share with our provider partner any information you provide, or that is collected, in the course of visiting any pages that are made available in cooperation with our provider partner. In some cases, the provider partner may collect information from you directly; in which cases the privacy policy of the provider partner may apply to the provider partner's use of your information. The privacy policy of our provider partners may differ from ours. If you have any questions regarding the privacy policy of one of our provider partners, you should contact the provider partner directly for more information.
+	</p>
+
+	<p>
+		Please be aware that we may occasionally release information about visitors if required to do so by law or if, in our business judgment, such disclosure is reasonably necessary: (a) to comply with legal process; (b) to enforce our Terms of Service; or (c) to protect the rights, property, or personal safety of our Site, us, our affiliates, our officers, directors, employees, representatives, our licensors, other users, and/or the public.<br/><br/>
+		Please also note that as our business grows, we may buy or sell various assets. In the unlikely event that we sell some or all of our assets, or our Site, is acquired by another company, information about our Site users may be among the transferred assets. 
+	</p>
+
+	<h2>Data Collected in Connection with Ad Serving and Targeting</h2>
+	<p>
+		We may use cookies, web beacons, pixel tags and similar technologies, and/or a third-party ad serving software, to collect non-personally identifiable information about site users and site activity, and we may use this information to, among other things, serve targeted advertisements on this site. The information collected allows us to analyze how users use the site and to track user interests, trends and patterns, thus allowing us to deliver more relevant advertisements to users.<br/><br/>
+		We also may use third-party service providers to target and serve some of the advertisements you see on the pages of our Site, and these providers likewise may use their own cookies, web beacons, pixel tags and similar technologies to collect non-personally identifiable information from our Site. These service providers may use that information, sometimes in conjunction with similar non-personally identifiable information gathered through other websites, to deliver advertisements on this Site, and on other websites that participate in our service providers’ advertising networks, that are tailored to match the perceived interests of consumers. This information also may be used to help measure and research an advertisement’s effectiveness, or for other purposes.<br/><br/>
+		The data collected in connection with the ad serving and ad targeting on our Site does not identify you personally and does not include your name, address, email address or telephone number, but it may include the IP address of your computer.<br/><br/>
+		End users are able to opt out of behavioral targeting by clicking on this link: http://www.networkadvertising.org/managing/opt_out.asp We take part in the Digital Advertising Alliance's (DAA) Self-Regulatory Program for Online Behavioral Advertising. Users can click on this link for more info about Ad Choices: http://www.aboutads.info/choices/.
+	</p>
+
+	<h2>Children's Privacy Statement</h2>
+	<p>
+		This children's privacy statement explains our practices with respect to the online collection and use of personal information from children under the age of 13.<br/><br/>
+		This Site is not directed to children under the age of thirteen and we do NOT knowingly collect personally identifiable information from children under the age of 13 as part of the Site. We screen users who wish to provide personal information in order to prevent users under the age of 13 from providing such information. If we become aware that we have inadvertently received personally identifiable information from a user under the age of 13 as part of the Site, we will delete such information from our records. Because we do not collect any personally identifiable information from children under the age of 13 via the Site, we also do NOT knowingly distribute such information to third parties. We do NOT knowingly allow children under the age of 13 to publicly post or otherwise distribute personally identifiable contact information through the Site.<br/><br/>
+		Because we do not collect any personally identifiable information from children under the age of 13 via the Site, we do NOT condition the participation of a child under 13 in the Site's online activities on providing personally identifiable information. 
+	</p>
+
+	<h2>How To Make Changes to Your Information</h2>
+	<p>
+		If you are a registered member of our Site, you can make changes to your account information by logging in to the Site and using the account tools available via the Site. If you have subscribed to one or more of our email newsletters, you may also change your subscriber information, modify your subscriptions, and/or unsubscribe from these newsletters at any time by visiting your account member's editing form. 
+	</p>
+	
+	<h2>Storage of Information</h2>
+	<p>
+		All information we gather on our Site is stored within a database to which only we and our hosting services provider are provided access. However, as effective as the reasonable security measures implemented by us may be, no physical or electronic security system is impenetrable. We cannot guarantee the security of our Site’s servers or databases, nor can we guarantee that information you supply will not be intercepted while being transmitted to us over the Internet 
+	</p>
+	
+	<h2>Questions Regarding Privacy</h2>
+	<p>
+		If you have questions or concerns about this Privacy Policy, our privacy practices, or your dealings with us, please contact us by writing to: ".HTML::email('support@'.Config::get("app.domain"))."
+	</p>",
+);
