7380e7642c5944d4fc3205228b55fd10d5fdfdae
Antoine Augusti <antoine@augusti.fr>
2014-06-27 19:07:34 +0200

    Update the welcome e-mail

--- app/views/emails/welcome.blade.php
+++ app/views/emails/welcome.blade.php
@@ -1,11 +1,22 @@
 @extends('emails/default')
 <?php
-$linkEditProfile = URL::route('users.edit', array($login));
-$linkCampaignText = TextTools::linkCampaign($linkEditProfile, 'callToEditProfile', 'email', 'welcome', 'linkBodyEmail');
+$urlEditProfile = URL::route('users.edit', array($login));
+$urlCampaignEditProfile = TextTools::linkCampaign($urlEditProfile, 'callToEditProfile', 'email', 'welcome', 'linkBodyEmail');
+
+$urlProfile = URL::route('users.show', array($login));
+$urlCampaignProfile = TextTools::linkCampaign($urlProfile, 'callToProfile', 'email', 'welcome', 'linkBodyEmail');
+
+$data = [
+	'login'                  => $login,
+	'urlCampaignEditProfile' => $urlCampaignEditProfile,
+	'urlEditProfile'         => $urlEditProfile,
+	'urlCampaignProfile'     => $urlCampaignProfile,
+	'urlProfile'             => $urlProfile,
+];
 ?>
 
 @section('content')
 	{{ Lang::get('auth.welcomeEmailWithUsername', array('login' => $login)) }}
 	<br/><br/>
-	{{ Lang::get('auth.bodyWelcomeEmail', array('login' => $login, 'linkEditProfile' => $linkCampaignText, 'url' => $linkEditProfile)) }}
+	{{ Lang::get('auth.bodyWelcomeEmail', $data) }}
 @stop
