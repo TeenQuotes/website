90cffddeb375e5e0eb05692d1e5f184c948798ad
Antoine Augusti <antoine@augusti.fr>
2014-06-27 15:21:39 +0200

    Welcome tutorial on the user's profile

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -1,16 +1,21 @@
 @extends('layouts/page')
 @section('content')
 	<div id="user-profile">
+		<!-- Alert if the profile is hidden -->
 		@if ($user->isHiddenProfile() AND Auth::check() AND Auth::user()->login == $user->login)
 			<div class="alert alert-info alert-dismissable no-hide">
 				<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 				<i class="fa fa-info"></i> {{ Lang::get('users.profileCurrentlyHidden') }}
 			</div>
 		@endif
+		
+		<!-- Profile basic info and counters -->
 		@include('users.profile.info')
 
+		<!-- Controls buttons -->
 		@include('users.profile.controls')
 
+		<!-- Content: quotes or comments -->
 		@if (!empty($quotes))
 			@if ($type == 'favorites')
 				<h2><i class="fa fa-heart"></i> {{ Lang::get('users.favoriteQuotes') }}</h2>
@@ -37,6 +42,11 @@
 			<div class="text-center">
 				{{ $paginator->links() }}
 			</div>
+		@else
+			<!-- If the user is new and is viewing its own profile, a small welcome tutorial -->
+			@if (Auth::check() AND Auth::user()->login == $user->login)
+				@include('users.welcome')
+			@endif
 		@endif
 	</div>
 @stop
---
+++ app/views/users/welcome.blade.php
@@ -0,0 +1,19 @@
+<div id="welcome-profile">
+	{{ $welcomeText }}
+	<div class="row">
+		<div class="col-md-6 col-xs-12">
+			<h3>{{ $updateProfileTitle }}</h3>
+			{{ $updateProfileContent }}
+		</div>
+
+		<div class="col-md-6 col-xs-12">
+			<h3>{{ $addingQuoteTitle }}</h3>
+			{{ $addingQuoteContent }}
+		</div>
+
+		<div class="col-md-6 col-xs-12">
+			<h3>{{ $addingFavoritesTitle }}</h3>
+			{{ $addingFavoritesContent }}
+		</div>
+	</div>
+</div>
