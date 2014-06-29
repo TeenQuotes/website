90b7212748537bae78c4f29be51c551448ab47aa
Antoine Augusti <antoine@augusti.fr>
2014-06-29 11:31:58 +0200

    Deep linking for the user's profile

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -174,6 +174,7 @@ class UsersController extends \BaseController {
 		$data['quotesPublishedCount'] = $user->getPublishedQuotesCount();
 		$data['favCount']             = $user->getFavoriteCount();
 		$data['viewingSelfProfile']   = $viewingSelfProfile;
+		$data['showType']             = ($type === false) ? 'published' : $type;
 
 		return View::make('users.show', $data);
 	}
--- app/lib/TeenQuotes/Composers/Users/ProfileComposer.php
+++ app/lib/TeenQuotes/Composers/Users/ProfileComposer.php
@@ -3,31 +3,38 @@ namespace TeenQuotes\Composers\Users;
 
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Route;
+use TeenQuotes\Composers\AbstractDeepLinksComposer;
 
-class ProfileComposer {
+class ProfileComposer extends AbstractDeepLinksComposer {
 
-    public function compose($view)
-    {
-        $viewData = $view->getData();
-        $login = $viewData['user']->login;
+	public function compose($view)
+	{
+		$viewData = $view->getData();
+		$login = $viewData['user']->login;
+		$showType = $viewData['showType'];
 
-        $welcomeText = Lang::get('users.newUserWelcomeProfile', ['login' => $login]);
-        
-        $updateProfileTitle = Lang::get('users.newUserTutorialProfileTitle');
-        $updateProfileContent = Lang::get('users.newUserTutorialProfileContent', ['url' => URL::route('users.edit', $login)]);
-        
-        $addingQuoteTitle = Lang::get('users.newUserTutorialAddingQuoteTitle');
-        $addingQuoteContent = Lang::get('users.newUserTutorialAddingQuoteContent', ['url' => URL::route('addquote')]);
-        
-        $addingFavoritesTitle = Lang::get('users.newUserTutorialFavoritesTitle');
-        $addingFavoritesContent = Lang::get('users.newUserTutorialFavoritesContent');
-        
-        $view->with('welcomeText', $welcomeText);
-        $view->with('updateProfileTitle', $updateProfileTitle);
-        $view->with('updateProfileContent', $updateProfileContent);
-        $view->with('addingQuoteTitle', $addingQuoteTitle);
-        $view->with('addingQuoteContent', $addingQuoteContent);
-        $view->with('addingFavoritesTitle', $addingFavoritesTitle);
-        $view->with('addingFavoritesContent', $addingFavoritesContent);
-    }
+		$welcomeText = Lang::get('users.newUserWelcomeProfile', ['login' => $login]);
+		
+		$updateProfileTitle = Lang::get('users.newUserTutorialProfileTitle');
+		$updateProfileContent = Lang::get('users.newUserTutorialProfileContent', ['url' => URL::route('users.edit', $login)]);
+		
+		$addingQuoteTitle = Lang::get('users.newUserTutorialAddingQuoteTitle');
+		$addingQuoteContent = Lang::get('users.newUserTutorialAddingQuoteContent', ['url' => URL::route('addquote')]);
+		
+		$addingFavoritesTitle = Lang::get('users.newUserTutorialFavoritesTitle');
+		$addingFavoritesContent = Lang::get('users.newUserTutorialFavoritesContent');
+		
+		// Content
+		$view->with('welcomeText', $welcomeText);
+		$view->with('updateProfileTitle', $updateProfileTitle);
+		$view->with('updateProfileContent', $updateProfileContent);
+		$view->with('addingQuoteTitle', $addingQuoteTitle);
+		$view->with('addingQuoteContent', $addingQuoteContent);
+		$view->with('addingFavoritesTitle', $addingFavoritesTitle);
+		$view->with('addingFavoritesContent', $addingFavoritesContent);
+
+		// For deep links
+		$view->with('deepLinksArray', $this->createDeepLinks('users/'.$login.'/'.$showType));
+	}
 }
