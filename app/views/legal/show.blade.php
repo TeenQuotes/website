c3dcef6106d5e5b105f8ff49bca41d54b3929bbc
Antoine Augusti <antoine@augusti.fr>
2014-06-29 15:58:49 +0200

    Animation for the legal page

--- app/views/legal/show.blade.php
+++ app/views/legal/show.blade.php
@@ -2,13 +2,15 @@
 
 @section('content')
 	<div id="legal-page">
-		<div id="ariane-line">
+		<div id="ariane-line" class="animated fadeInDown">
 			@foreach ($arianeLineLinks as $key => $value)
 				<a href="{{ URL::route('legal.show', $key) }}"><i class="fa fa-legal"></i>{{ $value }}</a>
 			@endforeach
 		</div>
-
-		<h1><i class="fa fa-legal"></i> {{ $title }}</h1>
-		{{ $content }}
+		
+		<div class="animated fadeInUp">		
+			<h1><i class="fa fa-legal"></i> {{ $title }}</h1>
+			{{ $content }}
+		</div>
 	</div>
 @stop
