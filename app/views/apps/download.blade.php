c2a10c94736b82acaa74f171503fb935c3e01a6a
Antoine Augusti <antoine@augusti.fr>
2014-06-29 15:54:37 +0200

    Add animation for the download apps page

--- app/views/apps/download.blade.php
+++ app/views/apps/download.blade.php
@@ -2,15 +2,15 @@
 
 @section('content')
 	<div id="download-app">
-		<div id="ariane-line">
+		<div id="ariane-line" class="animated fadeInDown">
 			@foreach ($devicesInfo as $deviceInfo)
 				@if (strtolower($deviceInfo['name']) != $deviceType)
 					<a href="{{ URL::route('apps.device', strtolower($deviceInfo['name'])) }}"><i class="fa {{ $deviceInfo['mini-icon'] }}"></i>{{ $deviceInfo['name'] }}</a>
 				@endif
 			@endforeach
 		</div>
-		<h2><i class="fa {{ $titleIcon}}"></i> {{ $title }}</h2>
-		<div class="row">
+		<h2 class="animated fadeInUp"><i class="fa {{ $titleIcon}}"></i> {{ $title }}</h2>
+		<div class="row animated fadeInUp">
 			<div class="col-md-6 col-sm-6 col-xs-8">
 				{{ $content }}
 			</div>
