c0e677659e30efbd6f13634734f52291f183fe43
Antoine Augusti <antoine@augusti.fr>
2014-07-04 21:29:27 +0200

    New way to inline CSS when sending an email

--- app/lib/TeenQuotes/Mail/MailServiceProvider.php
+++ app/lib/TeenQuotes/Mail/MailServiceProvider.php
@@ -1,23 +1,41 @@
 <?php
 namespace TeenQuotes\Mail;
-
-use TeenQuotes\Mail\Transport\MailgunTransport;
  
 class MailServiceProvider extends \Illuminate\Mail\MailServiceProvider {
  
-	/**
-	 * Register the Mailgun Swift Transport instance.
-	 *
-	 * @param  array  $config
-	 * @return void
-	 */
-	protected function registerMailgunTransport($config)
+	public function register()
 	{
-		$mailgun = $this->app['config']->get('services.mailgun', array());
-
-		$this->app->bindShared('swift.transport', function() use ($mailgun)
+		$this->registerSwiftMailer();
+ 
+		$this->app['mailer'] = $this->app->share(function($app)
 		{
-			return new MailgunTransport($mailgun['secret'], $mailgun['domain']);
+			// Once we have create the mailer instance, we will set a container instance
+			// on the mailer. This allows us to resolve mailer classes via containers
+			// for maximum testability on said classes instead of passing Closures.
+			$mailer = new Mailer($app['view'], $app['swift.mailer']);
+ 
+			$mailer->setLogger($app['log'])->setQueue($app['queue']);
+ 
+			$mailer->setContainer($app);
+ 
+			// If a "from" address is set, we will set it on the mailer so that all mail
+			// messages sent by the applications will utilize the same "from" address
+			// on each one, which makes the developer's life a lot more convenient.
+			$from = $app['config']['mail.from'];
+ 
+			if (is_array($from) and isset($from['address']))
+			{
+				$mailer->alwaysFrom($from['address'], $from['name']);
+			}
+ 
+			// Here we will determine if the mailer should be in "pretend" mode for this
+			// environment, which will simply write out e-mail to the logs instead of
+			// sending it over the web, which is useful for local dev enviornments.
+			$pretend = $app['config']->get('mail.pretend', false);
+ 
+			$mailer->pretend($pretend);
+ 
+			return $mailer;
 		});
 	}
 }
---
+++ app/lib/TeenQuotes/Mail/Mailer.php
@@ -0,0 +1,29 @@
+<?php
+namespace TeenQuotes\Mail;
+
+use TijsVerkoyen\CssToInlineStyles\CssToInlineStyles;
+
+class Mailer extends \Illuminate\Mail\Mailer {
+ 
+	/**
+	 * Render the given view.
+	 *
+	 * @param  string  $view
+	 * @param  array   $data
+	 * @return \Illuminate\View\View
+	 */
+	protected function getView($view, $data)
+	{
+		$cssInline = new CssToInlineStyles();
+ 
+		$view = $this->views->make($view, $data)->render(); 
+		
+		// Inline CSS
+		$cssInline->setUseInlineStylesBlock();
+		$cssInline->setStripOriginalStyleTags();
+		$cssInline->setCleanup();
+		$cssInline->setHTML($view);
+ 
+		return $cssInline->convert();
+	}
+}
