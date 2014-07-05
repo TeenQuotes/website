95e7bcfabe61d694b6031d844fdc8b38733afe9c
Antoine Augusti <antoine@augusti.fr>
2014-07-05 16:12:00 +0200

    Fix URL sent to reset a password

--- app/views/emails/auth/reminder.blade.php
+++ app/views/emails/auth/reminder.blade.php
@@ -3,5 +3,5 @@
 @section('content')
 	{{ Lang::get('auth.heyEmail') }}<br/>
 	<br/>
-	{{ Lang::get('auth.askedResetPasswordEmail') }} {{ URL::to('password/reset', array($token)) }}.
+	{{ Lang::get('auth.askedResetPasswordEmail') }} {{ URL::action('RemindersController@getReset', compact('token')) }}.
 @stop
