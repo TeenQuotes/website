4a2672bbff7c33b7e1fdcc13d32f5512125fb6e2
Antoine Augusti <antoine@augusti.fr>
2014-07-20 18:58:32 +0200

    Coding style

--- app/models/Quote.php
+++ app/models/Quote.php
@@ -352,19 +352,18 @@ class Quote extends Toloquent {
 
     	// Format the hex color string
 		$hex = str_replace('#', '', $hex);
-		if (strlen($hex) == 3) {
-			$hex = str_repeat(substr($hex,0,1), 2).str_repeat(substr($hex,1,1), 2).str_repeat(substr($hex,2,1), 2);
-		}
+		if (strlen($hex) == 3)
+			$hex = str_repeat(substr($hex, 0, 1), 2).str_repeat(substr($hex, 1, 1), 2).str_repeat(substr($hex, 2, 1), 2);
 
     	// Get decimal values
-		$r = hexdec(substr($hex,0,2));
-		$g = hexdec(substr($hex,2,2));
-		$b = hexdec(substr($hex,4,2));
+		$r = hexdec(substr($hex, 0, 2));
+		$g = hexdec(substr($hex, 2, 2));
+		$b = hexdec(substr($hex, 4, 2));
 
     	// Adjust number of steps and keep it inside 0 to 255
-		$r = max(0, min(255,$r + $steps));
-		$g = max(0, min(255,$g + $steps));
-		$b = max(0, min(255,$b + $steps));
+		$r = max(0, min(255, $r + $steps));
+		$g = max(0, min(255, $g + $steps));
+		$b = max(0, min(255, $b + $steps));
 
 		$r_hex = str_pad(dechex($r), 2, '0', STR_PAD_LEFT);
 		$g_hex = str_pad(dechex($g), 2, '0', STR_PAD_LEFT);
