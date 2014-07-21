0ff7b9f9d871f52e7be9f8bf682a6fa50b6d7b27
Antoine Augusti <antoine@augusti.fr>
2014-07-21 13:31:10 +0200

    Extra line

--- app/views/auth/signupCall.blade.php
+++ app/views/auth/signupCall.blade.php
@@ -1,6 +1,7 @@
 <?php
 $addClass = Session::has('requireLoggedInAddQuote') ? ' col-md-push-6' : '';
 ?>
+
 <!-- CALL TO SIGN UP -->
 <div class="animated fadeInRight col-md-6<?= $addClass ?>">
 	<h1>{{ Lang::get('auth.wantToBeCool') }}</h1>
