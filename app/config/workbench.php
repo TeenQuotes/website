b492d4154950f95259720e02c2ba01f37d12a123
Antoine Augusti <antoine@augusti.fr>
2014-07-23 15:48:40 +0200

    Configure Workbench

--- app/config/workbench.php
+++ app/config/workbench.php
@@ -13,7 +13,7 @@ return array(
 	|
 	*/
 
-	'name' => '',
+	'name' => 'Antoine Augusti',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -26,6 +26,6 @@ return array(
 	|
 	*/
 
-	'email' => '',
+	'email' => 'antoine.augusti@gmail.com',
 
 );
