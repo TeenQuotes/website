0734b07c7dbe27b5d73eac3656ad34f01a7d7627
Antoine Augusti <antoine@augusti.fr>
2014-07-26 22:04:53 +0200

    Do not link to the quote if the quote was not approved

--- app/views/emails/quotes/single.blade.php
+++ app/views/emails/quotes/single.blade.php
@@ -1,9 +1,15 @@
 <div class="quote">
-	{{{ $quote['content']}}}
+	{{{ $quote->content}}}
 	<div class="info">
-		<a href="{{ URL::route('quotes.show', array($quote['id'])) }}">#{{{ $quote['id'] }}}</a>
+		@if ($quote->isPublished())
+			<a href="{{ URL::route('quotes.show', array($quote->id)) }}">
+		@endif
+			#{{{ $quote->id }}}
+		@if ($quote->isPublished())
+			</a>
+		@endif
 		<div class="author">
-			<a href="{{ URL::route('users.show', array($quote['user']['login'])) }}">{{{ $quote['user']['login'] }}}</a>
+			<a href="{{ URL::route('users.show', array($quote->user->login)) }}">{{{ $quote->user->login }}}</a>
 		</div>
 	</div>
 </div>
