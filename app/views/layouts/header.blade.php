7fab6c308f6a734d41f7f6c5e250ca5cceffed9c
Antoine Augusti <antoine@augusti.fr>
2014-07-30 09:36:35 +0200

    Do not display a meta description if we haven't a description

--- app/views/layouts/header.blade.php
+++ app/views/layouts/header.blade.php
@@ -4,7 +4,9 @@
 	<meta charset="utf-8">
 	<title>{{ isset($pageTitle) ? $pageTitle : Lang::get('layout.nameWebsite') }}</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="{{isset($pageDescription) ? $pageDescription : ''}}">
+	@if (isset($pageDescription))
+		<meta name="description" content="{{ $pageDescription }}">
+	@endif
 	{{ HTML::style('assets/css/styles.min.css') }}
 	<link rel="shortcut icon" href="/assets/images/favicon.png"/>
 
