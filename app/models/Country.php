6b610ba38f2dc914012ef8d28bad57820e357b32
Antoine Augusti <antoine@augusti.fr>
2014-08-04 12:28:12 +0200

    Update the accessor of the default country

--- app/models/Country.php
+++ app/models/Country.php
@@ -6,10 +6,9 @@ class Country extends \Eloquent {
 	protected $fillable = ['name'];
 
 	/**
-	 * The ID of the default country to use. This is the United States
-	 * @var int
+	 * The ID of the United States
 	 */
-	const DEFAULT_COUNTRY = 224;
+	const ID_UNITED_STATES = 224;
 
 	public function users()
 	{
@@ -18,6 +17,11 @@ class Country extends \Eloquent {
 
 	public static function getDefaultCountry()
 	{
-		return self::DEFAULT_COUNTRY;
+		// If we have the information in settings.json, return it
+		if (LaraSetting::has('countries.defaultCountry'))
+			return LaraSetting::get('countries.defaultCountry');
+		// We have no clue, return the ID of the USA
+		else
+			return self::ID_UNITED_STATES;
 	}
 }
