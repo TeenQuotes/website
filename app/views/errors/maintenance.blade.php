97bbba412452348937413a456e7f28544ff2cd69
Antoine Augusti <antoine@augusti.fr>
2014-08-06 14:16:46 +0200

    Maintenance view

---
+++ app/views/errors/maintenance.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Be right back!</title>
+	<link href='http://fonts.googleapis.com/css?family=PT+Mono' rel='stylesheet' type='text/css'>
+	<meta name="viewport" content="user-scalable=no, initial-scale = 1, minimum-scale = 1, maximum-scale=1, width=device-width">
+
+	<style>
+		* { margin: 0; padding: 0; }
+		html, body {height: 100%;background: #313131;color: #FFF;font-family:'PT Mono', Arial;}
+		.container {position: absolute;width: 100%;height: 100%;display: table;}
+		span {height: 100%;vertical-align: middle;display: inline-block; }
+		.middle {vertical-align: middle;display: table-cell;}
+
+    	h1 {font-size: 2.5em; text-align: center; margin-bottom:50px;}
+    	p {margin: 0 auto 20px auto;padding: 0;font-size: 2em;max-width: 80%;line-height: 1.5}
+    	a {color: #CCC;}
+
+    	@media only screen and (max-width: 767px) {
+			h1 {font-size: 2em;}
+			p {font-size: 1.5em;}
+		}
+		@media only screen and (max-width: 400px) {
+			h1 {font-size: 1.8em;}
+			p {font-size: 1.4em;}
+		}
+	</style>
+</head>
+
+<body>
+	<div class="container">
+	    <div class="middle">
+	    	<h1>Under maintenance</h1>
+	    	<p>
+	    		Sorry, we are not available for the moment.
+	    	</p>
+	    	<p>
+	    		Our engineering team is already working on getting things back! Be sure to come back soon.
+	    	</p>
+	    </div>
+	</div>
+</body>
+</html>
