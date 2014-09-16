13faf400f6f38a6899ec665c1bfd07cd0778dded
Antoine Augusti <antoine@augusti.fr>
2014-09-16 14:26:15 +0200

    Configuration file for philo/translate

---
+++ app/config/packages/philo/translate/config.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	'search_ignore_folders' => array('config', 'database', 'lang', 'start', 'storage', 'tests'),
+	'search_exclude_files'  => array('pagination', 'reminders', 'validation'),
+	'digg_folders'          => array('app/models', 'app/views', 'app/controllers', 'app/commands'),
+);
