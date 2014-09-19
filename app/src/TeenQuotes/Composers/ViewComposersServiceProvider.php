1591045a8a5523ea20bab697cb042296a15fd0a6
Antoine Augusti <antoine@augusti.fr>
2014-09-19 23:18:02 +0200

    Create a ViewComposersServiceProvider

---
+++ app/src/TeenQuotes/Composers/ViewComposersServiceProvider.php
@@ -0,0 +1,76 @@
+<?php namespace TeenQuotes\Composers;
+
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class ViewComposersServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		//
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		// Show a user's profile
+		View::composer([
+			'users.show',
+			'users.welcome'
+		], 'TeenQuotes\Composers\Users\ProfileComposer');
+
+		// Self edit user's profile
+		View::composer([
+			'users.edit'
+		], 'TeenQuotes\Composers\Users\ProfileEditComposer');
+
+		// Reset a password with a token
+		View::composer([
+			'password.reset'
+		], 'TeenQuotes\Composers\Password\ResetComposer');
+
+		// View a single quote
+		View::composer([
+			'quotes.singleQuote'
+		], 'TeenQuotes\Composers\Quotes\SingleComposer');
+
+		// Associated URLs: ['home', 'contact', 'apps', 'signin', 'legal', 
+		// 'signup', 'password/remind', 'random', 'addquote'],
+		View::composer([
+			'quotes.index',
+			'contact.show',
+			'apps.download',
+			'auth.signin',
+			'legal.show',
+			'auth.signup',
+			'password.remind',
+			'quotes.addquote'
+		], 'TeenQuotes\Composers\Pages\SimplePageComposer');
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		//
+	}
+}
