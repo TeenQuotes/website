5be9190b675e95d7a6f191001630984b0798d9ba
Antoine Augusti <antoine@augusti.fr>
2014-09-20 11:27:45 +0200

    Clean emails.events.* views

--- app/views/emails/events/newyear.blade.php
+++ app/views/emails/events/newyear.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<img src="{{ Lang::get('email.imageNewyear') }}" id="img-event" />
-	{{ Lang::get('email.hiWithLogin', array('login' => $user['login'])) }}
+	{{ Lang::get('email.hiWithLogin', ['login' => $user->login]) }}
 	<br/><br/>
 	{{ Lang::get('email.newyearBody') }}
 @stop
