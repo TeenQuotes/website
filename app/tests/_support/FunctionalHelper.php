3a6c5eded58c6afdf3ab18e3b00983b6be99fdab
Antoine Augusti <antoine@augusti.fr>
2014-10-02 19:44:33 +0200

    Add a functional test to verify that a user can update its settings

--- app/tests/_support/FunctionalHelper.php
+++ app/tests/_support/FunctionalHelper.php
@@ -1,6 +1,8 @@
 <?php namespace Codeception\Module;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Laracasts\TestDummy\Factory as TestDummy;
@@ -30,6 +32,53 @@ class FunctionalHelper extends \Codeception\Module
 		$this->seeSuccessFlashMessage('Nice to see you :)');
 		$I->see('My profile', '.navbar');
 		$I->assertTrue(Auth::check());
+	}
+
+	public function assertMySettingsHaveDefaultValues()
+	{
+		$this->assertMySettingsHaveTheseValues([
+			'color'                      => ucfirst(Config::get('app.users.defaultColorQuotesPublished')),
+			// Receive a notification if a comment is posted on one of my quotes
+			'notification_comment_quote' => 1,
+			// Profile not hidden
+			'hide_profile'               => 0,
+			// Subscribed to the weekly newsletter
+			'daily_newsletter'           => 0,
+			'weekly_newsletter'          => 1
+		]);
+	}
+
+	public function assertMySettingsHaveTheseValues(array $params)
+	{
+		$I = $this->getModule('Laravel4');
+		
+		$I->seeOptionIsSelected('select[name=colors]', $params['color']);
+		
+		foreach (['notification_comment_quote', 'hide_profile', 'daily_newsletter', 'weekly_newsletter'] as $value) {
+			if ($params[$value] == 1)
+				$I->seeCheckboxIsChecked('input[name='.$value.']');
+			else 
+				$I->dontSeeCheckboxIsChecked('input[name='.$value.']');
+		}
+	}
+
+	public function fillUserSettingsForm(array $params)
+	{
+		$I = $this->getModule('Laravel4');
+		
+		$I->selectOption('select[name=colors]', $params['color']);
+
+		foreach (['notification_comment_quote', 'hide_profile', 'daily_newsletter', 'weekly_newsletter'] as $value) {
+			if ($params[$value] == 1)
+				$I->checkOption('input[name='.$value.']');
+			else 
+				$I->uncheckOption('input[name='.$value.']');
+		}
+
+		// Submit the form
+		$I->click('Edit my settings!');
+		$I->seeCurrentRouteIs('users.edit', Auth::user()->login);
+		$I->see('Your settings have been changed');
 	}
 
 	public function navigateToTheSignInPage()
@@ -254,6 +303,72 @@ class FunctionalHelper extends \Codeception\Module
 		$I = $this->getModule('Laravel4');
 		
 		$I->see($message, '.alert-success');
+	}
+
+	public function assertEditProfileFormIsFilledWith(array $params)
+	{
+		$I = $this->getModule('Laravel4');
+
+		$I->seeOptionIsSelected('input[name=gender]', $params['gender']);
+		$I->see($params['birthdate']);
+		$I->seeOptionIsSelected('select[name=country]', $params['country_name']);
+		$I->see($params['city']);
+		$I->see($params['about_me']);
+	}
+
+	/**
+	 * Fill the edit profile form with the given key-value pairs
+	 * @param  array  $params The key-values pairs. Required keys: gender, birthdate (YYYY-MM-DD), country_name, city, about_me
+	 */
+	public function fillEditProfileFormWith(array $params)
+	{
+		$I = $this->getModule('Laravel4');
+
+		$I->selectOption('input[name=gender]', $params['gender']);
+		$I->fillField('Birthdate', $params['birthdate']);
+		$I->selectOption('select[name=country]', $params['country_name']);
+		$I->fillField('City', $params['city']);
+		$I->fillField('About me', $params['about_me']);
+
+		$I->click('Edit my profile!');
+	}
+
+	/**
+	 * Assert that the logged in user has got the given key-values pairs for its profile
+	 * @param  array  $params The key-values pairs. Required keys: gender, birthdate (YYYY-MM-DD), country_name, city, about_me
+	 */
+	public function assertProfileHasBeenChangedWithParams(array $params)
+	{
+		$I = $this->getModule('Laravel4');
+		
+		$I->seeCurrentRouteIs('users.edit', Auth::user()->login);
+		$this->seeSuccessFlashMessage('You have a brand new profile');
+
+		$this->navigateToMyProfile();
+
+		$I->see($params['country_name']);
+		$I->see($params['city']);
+		$I->see($params['about_me']);
+		$age = $this->computeAgeFromYYYMMDD($params['birthdate']);
+		$I->see($age.' y/o');
+
+		if ($params['gender'] == 'M')
+			$I->see("I'm a man");
+		else
+			$I->see("I'm a woman");
+	}
+
+	/**
+	 * Compute the age from a date formatted as YYYY-MM-DD
+	 * @param  string $date The date formatted as YYYY-MM-DD
+	 * @return int The age
+	 */
+	private function computeAgeFromYYYMMDD($date)
+	{
+		// Create an array to have year, month and day
+		$parts = explode('-', $date);
+		
+		return Carbon::createFromDate($parts[0], $parts[1], $parts[2])->age;
 	}
 
 	/**
---
+++ app/tests/functional/User/UpdateSettingsCest.php
@@ -0,0 +1,43 @@
+<?php
+
+class UpdateSettingsCest {
+
+	/**
+	 * The authenticated user
+	 * @var User
+	 */
+	private $user;
+
+	public function _before(FunctionalTester $I)
+	{
+		$this->user = $I->logANewUser();
+		// Create some published quotes for the logged in user
+		$I->createSomePublishedQuotes(['user_id' => $this->user->id]);
+	}
+
+	public function updateMySettings(FunctionalTester $I)
+	{
+		$I->am('a logged in Teen Quotes member');
+		$I->wantTo('update my settings');
+
+		$I->navigateToMyEditProfilePage();
+		$I->assertMySettingsHaveDefaultValues();
+
+		$newColor = 'red';
+		$params = [
+			'color'                      => ucfirst($newColor),
+			'notification_comment_quote' => 0,
+			'hide_profile'               => 1,
+			'daily_newsletter'           => 1,
+			'weekly_newsletter'          => 0
+		];
+		$I->fillUserSettingsForm($params);
+		$I->assertMySettingsHaveTheseValues($params);
+
+		$I->navigateToMyProfile();
+		// Verify that the profile is hidden
+		$I->see('Your profile is currently hidden. Only you can see this!');
+		// Verify the color of published quotes
+		$I->seeElement('.color-'.$newColor.'-1');
+	}
+}
