485dcd1f97160b158c170f1ceb6dfa0c0756cda5
Antoine Augusti <antoine@augusti.fr>
2014-10-19 18:22:42 +0200

    Add the edit link to the comment view and create methods for the controller

--- app/TeenQuotes/Comments/Controllers/CommentsController.php
+++ app/TeenQuotes/Comments/Controllers/CommentsController.php
@@ -3,12 +3,14 @@
 use BaseController;
 use Illuminate\Http\Response as ResponseClass;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\View;
 use TeenQuotes\Comments\Models\Comment;
 
 class CommentsController extends BaseController {
@@ -32,26 +34,68 @@ class CommentsController extends BaseController {
 	 */
 	public function store()
 	{
-		$data = [
-			'content'  => Input::get('content'),
-			'quote_id' => Input::get('quote_id'),
-		];
+		$data = Input::only('content', 'quote_id');
 
+		// Check if the form validates with success
 		$validator = Validator::make($data, Comment::$rulesAdd);
-
-		// Check if the form validates with success.
 		if ($validator->passes()) {
 
 			// Call the API - skip the API validator
 			$response = $this->api->store($data['quote_id'], false);
 			if ($response->getStatusCode() != 201)
-				return Redirect::route('quotes.show', ['id' => $data['quote_id']])->withErrors($validator)->withInput(Input::all());
+				return Redirect::route('quotes.show', $data['quote_id'])->withErrors($validator)->withInput(Input::all());
 
-			return Redirect::route('quotes.show', ['id' => $data['quote_id']])->with('success', Lang::get('comments.commentAddedSuccessfull'));
+			return Redirect::route('quotes.show', $data['quote_id'])->with('success', Lang::get('comments.commentAddedSuccessfull'));
 		}
 
-		// Something went wrong.
-		return Redirect::route('quotes.show', ['id' => $data['quote_id']])->withErrors($validator)->withInput(Input::all());
+		// Something went wrong
+		return Redirect::route('quotes.show', $data['quote_id'])->withErrors($validator)->withInput(Input::all());
+	}
+
+	/**
+	 * Show the form to edit a comment
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		$comment = $this->api->show($id)->getOriginalData();
+
+		// If the comment was not found or is not posted by the user
+		if (is_null($comment) OR ! $comment->isPostedBySelf())
+			return Redirect::home()->with('warning', Lang::get('comments.cantEditThisComment'));
+
+		$data = compact('comment');
+		$data['pageTitle'] = Lang::get('comments.updateCommentPageTitle');
+
+		return View::make('comments.edit', $data);
+	}
+
+	/**
+	 * Edit a comment
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		$data = Input::only('content', 'quote_id');
+
+		// Check if the form validates with success
+		$validator = Validator::make($data, Comment::$rulesEdit);
+		if ($validator->passes()) {
+
+			// Call the API
+			$response = $this->api->update($id);
+			if ($response->getStatusCode() != 200)
+				return Redirect::route('quotes.show', $data['quote_id'])->with('warning', Lang::get('comments.cantEditThisComment'));
+
+			return Redirect::route('quotes.show', $data['quote_id'])->with('success', Lang::get('comments.commentEditSuccessfull'));
+		}
+
+		// Something went wrong
+		return Redirect::route('comments.edit', $id)->withErrors($validator)->withInput(Input::all());
 	}
 
 	/**
