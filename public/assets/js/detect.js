1df034167c641f94e2b8792d802eb60882e621db
Antoine Augusti <antoine@augusti.fr>
2014-12-06 21:20:03 +0100

    Log events in Google Analytics for ads. Close #40

--- public/assets/js/detect.js
+++ public/assets/js/detect.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
 	var div = '<div id="need-money">' + laravel.moneyDisclaimer + '</div>';
-	if (typeof(displayNeedMoney) == "undefined")
+	if (typeof(displayNeedMoney) == "undefined") {
 		$("#paginator-quotes").before(div);
+		ga('send', 'event', 'ads.quotes.index', 'hidden');
+	} else {
+		ga('send', 'event', 'ads.quotes.index', 'displayed');
+	}
 });
