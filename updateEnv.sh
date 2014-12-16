0831509afd92b555a86f2d127bfa384d68ac55e5
Antoine Augusti <antoine@augusti.fr>
2014-12-16 11:44:07 +0100

    Use phpdotenv to deal with environment variables

--- updateEnv.sh
+++ updateEnv.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# Remove old files
-rm -f .env.codeception.php
-rm -f .env.testing.php
-rm -f .env.codeceptionSearch.php
-
-# Create new files
-cp .env.local.php .env.codeception.php
-cp .env.local.php .env.testing.php
-cp .env.local.php .env.codeceptionSearch.php
-
 # Upload to production
-scp .env.local.php root@v3.tq:/var/www/website/.env.production.php
-ssh root@v3.tq "chown www-data:www-data /var/www/website/.env.production.php"
+scp .env root@v3.tq:/var/www/website/.env
+ssh root@v3.tq "chown www-data:www-data /var/www/website/.env"
