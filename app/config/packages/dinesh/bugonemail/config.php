d46697ab6a2b782360680ac94117e929f0303149
Antoine Augusti <antoine@augusti.fr>
2014-12-19 18:46:39 +0100

    Do not alert for MethodNotAllowedHttpException

--- app/config/packages/dinesh/bugonemail/config.php
+++ app/config/packages/dinesh/bugonemail/config.php
@@ -5,5 +5,5 @@ return array(
 	'notify_emails'      => ['antoine.augusti@teen-quotes.com'],
 	'email_template'     => "bugonemail::email.notifyException",
 	'notify_environment' => ['staging', 'production'],
-	'prevent_exception'  => ['Symfony\Component\HttpKernel\Exception\NotFoundHttpException'],
+	'prevent_exception'  => ['Symfony\Component\HttpKernel\Exception\NotFoundHttpException', 'Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException'],
 );
