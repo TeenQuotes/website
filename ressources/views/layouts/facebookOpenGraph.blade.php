fd167573757837dc64774411aff5fa901912b0ca
Antoine Augusti <antoine@augusti.fr>
2014-12-23 17:19:22 +0100

    Set the width and the height of the image of the card

--- ressources/views/layouts/facebookOpenGraph.blade.php
+++ ressources/views/layouts/facebookOpenGraph.blade.php
@@ -1,4 +1,4 @@
 <meta property="og:type" content="article" />
 <meta property="og:url" content="{{ URL::current() }}" />
 <meta property="og:title" content="{{ $pageTitle }}" />
-<meta property="og:image" content="{{ Lang::get('layout.imageFacebookOpenGraph') }}" />
+<meta property="og:image" content="{{ Lang::get('layout.cardURL') }}" />
--- ressources/views/layouts/twitterCard.blade.php
+++ ressources/views/layouts/twitterCard.blade.php
@@ -2,5 +2,7 @@
 <meta name="twitter:site" content="{{ Lang::get('layout.twitterUsername') }}">
 <meta name="twitter:title" content="Quote #{{ $quote->id }}">
 <meta name="twitter:description" content="{{ $quote->present()->textTwitterCard }}">
-<meta name="twitter:image:src" content="{{ Lang::get('layout.imageTwitterCard') }}">
+<meta name="twitter:image:src" content="{{ Lang::get('layout.cardURL') }}">
+<meta name="twitter:image:height" content="{{ Lang::get('layout.cardHeight') }}">
+<meta name="twitter:image:width" content="{{ Lang::get('layout.cardWidth') }}">
 <meta name="twitter:url" content="{{ URL::current() }}">
