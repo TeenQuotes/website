24f4a10593d97cb1c67403c0607173a4a3b7039c
Antoine Augusti <antoine@augusti.fr>
2014-12-24 17:45:22 +0100

    Update Twitter and Facebook cards

--- ressources/views/layouts/facebookOpenGraph.blade.php
+++ ressources/views/layouts/facebookOpenGraph.blade.php
@@ -1,4 +1,7 @@
+<meta property="og:description" content="{{ $quote->content }}" />
+<meta property="og:image" content="{{ Lang::get('layout.cardFacebookURL') }}" />
+<meta property="og:image:height" content="{{ Lang::get('layout.cardFacebookHeight') }}">
+<meta property="og:image:width" content="{{ Lang::get('layout.cardFacebookWidth') }}">
+<meta property="og:title" content="{{ $pageTitle }}" />
 <meta property="og:type" content="article" />
 <meta property="og:url" content="{{ URL::current() }}" />
-<meta property="og:title" content="{{ $pageTitle }}" />
-<meta property="og:image" content="{{ Lang::get('layout.cardURL') }}" />
--- ressources/views/layouts/twitterCard.blade.php
+++ ressources/views/layouts/twitterCard.blade.php
@@ -1,8 +1,8 @@
 <meta name="twitter:card" content="summary">
+<meta name="twitter:description" content="{{ $quote->present()->textTwitterCard }}">
+<meta name="twitter:image:height" content="{{ Lang::get('layout.cardTwitterHeight') }}">
+<meta name="twitter:image:src" content="{{ Lang::get('layout.cardTwitterURL') }}">
+<meta name="twitter:image:width" content="{{ Lang::get('layout.cardTwitterWidth') }}">
 <meta name="twitter:site" content="{{ Lang::get('layout.twitterUsername') }}">
 <meta name="twitter:title" content="Quote #{{ $quote->id }}">
-<meta name="twitter:description" content="{{ $quote->present()->textTwitterCard }}">
-<meta name="twitter:image:src" content="{{ Lang::get('layout.cardURL') }}">
-<meta name="twitter:image:height" content="{{ Lang::get('layout.cardHeight') }}">
-<meta name="twitter:image:width" content="{{ Lang::get('layout.cardWidth') }}">
 <meta name="twitter:url" content="{{ URL::current() }}">
