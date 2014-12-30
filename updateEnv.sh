7e1de97f439035cbb72ee2989ad061d76374c775
Antoine Augusti <antoine@augusti.fr>
2014-12-30 12:20:12 +0100

    Upload environement variables to staging as well

--- updateEnv.sh
+++ updateEnv.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Upload to production
-scp .env root@v3.tq:/var/www/website/.env
-ssh root@v3.tq "chown www-data:www-data /var/www/website/.env"
+scp .env root@production.tq:/var/www/website/.env
+ssh root@production.tq "chown www-data:www-data /var/www/website/.env"
+
+# Upload to staging
+scp .env admin@staging.tq:/home/admin/.env
+ssh admin@staging.tq "sudo mv -f /home/admin/.env /var/www/website/.env;sudo chown www-data:www-data /var/www/website/.env"
