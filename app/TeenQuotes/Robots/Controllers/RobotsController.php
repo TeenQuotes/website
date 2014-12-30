5f1fb63de05ca348e1294dd8c1d2847bf14e1460
Antoine Augusti <antoine@augusti.fr>
2014-12-30 14:47:02 +0100

    Do not use a static robots.txt file

---
+++ app/TeenQuotes/Robots/Controllers/RobotsController.php
@@ -0,0 +1,47 @@
+<?php namespace TeenQuotes\Robots\Controllers;
+
+use BaseController, Response;
+use Illuminate\Foundation\Application as App;
+use Healey\Robots\Robots;
+
+class RobotsController extends BaseController {
+
+	/**
+	 * @var Healey\Robots\Robots
+	 */
+	private $robots;
+
+	private $app;
+
+	public function __construct(Robots $robots, App $app)
+	{
+		$this->robots = $robots;
+		$this->app = $app;
+	}
+
+	public function getRobots()
+	{
+		$respnse = $this->constructResponse($this->app->environment());
+
+		return Response::make($this->robots->generate(), 200, ['Content-Type' => 'text/plain']);
+	}
+
+	private function constructResponse($env)
+	{
+		switch ($env)
+		{
+			// If on the live server, serve a nice, welcoming robots.txt.
+			case 'production':
+				$response = $this->robots->addUserAgent('*');
+				$response .= $this->robots->addAllow('/');
+				break;
+
+			// If you're on any other server, tell everyone to go away.
+			default:
+				$response = $this->robots->addUserAgent('*');
+				$response .= $this->robots->addDisallow('/');
+		}
+
+		return $this->robots->generate();
+	}
+}
