af6ffde6a8c413aeeba2ffa081263385b7ec4b28
Antoine Augusti <antoine@augusti.fr>
2015-01-10 19:23:21 +0100

    Log when a user view another profile

---
+++ app/TeenQuotes/Queues/Workers/ProfileVisitorWorker.php
@@ -0,0 +1,27 @@
+<?php namespace TeenQuotes\Queues\Workers;
+
+use TeenQuotes\Users\Repositories\ProfileVisitorRepository;
+
+class ProfileVisitorWorker {
+
+	/**
+	 * @var TeenQuotes\Users\Repositories\ProfileVisitorRepository
+	 */
+	private $repo;
+
+	public function __construct(ProfileVisitorRepository $repo)
+	{
+		$this->repo = $repo;
+	}
+
+	/**
+	 * View a user profile
+	 *
+	 * @param \Illuminate\Queue\Jobs\SyncJob $job
+	 * @param array $data Required keys: visitor_id and user_id.
+	 */
+	public function viewProfile($job, $data)
+	{
+		$this->repo->addVisitor($data['user_id'], $data['visitor_id']);
+	}
+}
--- app/TeenQuotes/Users/Composers/ShowComposer.php
+++ app/TeenQuotes/Users/Composers/ShowComposer.php
@@ -1,15 +1,25 @@
 <?php namespace TeenQuotes\Users\Composers;
 
+use Auth, Queue, TextTools;
 use Illuminate\Database\Eloquent\Collection;
+use TeenQuotes\Queues\Workers\ProfileVisitorWorker;
 use TeenQuotes\Quotes\Models\Quote;
 use TeenQuotes\Tools\Composers\Interfaces\QuotesColorsExtractor;
-use TextTools;
 
 class ShowComposer implements QuotesColorsExtractor {
-	
+
+	/**
+	 * The section of the profile we are viewing
+	 * @var string
+	 */
 	private $type;
+
+	/**
+	 * The user viewed
+	 * @var TeenQuotes\Users\Models\User
+	 */
 	private $user;
-	
+
 	public function compose($view)
 	{
 		$data = $view->getData();
@@ -21,9 +31,24 @@ class ShowComposer implements QuotesColorsExtractor {
 		$view->with('addedFavCount', TextTools::formatNumber($this->user->getAddedFavCount()));
 		$view->with('quotesPublishedCount', TextTools::formatNumber($this->user->getPublishedQuotesCount()));
 		$view->with('favCount', TextTools::formatNumber($this->user->getFavoriteCount()));
-		
+
 		// Extract colors for quotes
 		$view->with('colors', $this->extractAndStoreColors($data['quotes']));
+
+		// Register the visit of the profile
+		$this->registerVisit();
+	}
+
+	private function registerVisit()
+	{
+		if (Auth::check())
+		{
+			$user_id = $this->user->id;
+			$visitor_id = Auth::id();
+
+			if ($user_id != $visitor_id)
+				Queue::push(ProfileVisitorWorker::class.'@viewProfile', compact('user_id', 'visitor_id'));
+		}
 	}
 
 	public function extractAndStoreColors($quotes)
