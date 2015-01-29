9316853038d0eacbc1060b7b6256edb41386b721
Antoine Augusti <antoine@augusti.fr>
2015-01-29 19:12:05 +0100

    Create classes to detect the city and the country from a request

---
+++ app/TeenQuotes/Countries/Localisation/CityDetector.php
@@ -0,0 +1,16 @@
+<?php namespace TeenQuotes\Countries\Localisation;
+
+use Symfony\Component\HttpFoundation\Request;
+
+interface CityDetector {
+
+	/**
+	 * Detect the city for a HTTP request
+	 *
+	 * @param  \Symfony\Component\HttpFoundation\Request $request
+	 * @see    \TeenQuotes\Countries\Models\Country
+	 * @return string The name of the detected city
+	 * If we can't find a match, return null
+	 */
+	public function detectCity(Request $request);
+}
---
+++ app/TeenQuotes/Countries/Localisation/Detector.php
@@ -0,0 +1,32 @@
+<?php namespace TeenQuotes\Countries\Localisation;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class Detector implements CityDetector, CountryDetector {
+
+	/**
+	 * @var \TeenQuotes\Countries\Localisation\CityDetector
+	 */
+	private $cityDetector;
+
+	/**
+	 * @var \TeenQuotes\Countries\Localisation\CountryDetector
+	 */
+	private $countryDetector;
+
+	public function __construct(CityDetector $cityDetector, CountryDetector $countryDetector)
+	{
+		$this->cityDetector = $cityDetector;
+		$this->countryDetector = $countryDetector;
+	}
+
+	public function detectCountry(Request $request)
+	{
+		return $this->countryDetector->detectCountry($request);
+	}
+
+	public function detectCity(Request $request)
+	{
+		return $this->cityDetector->detectCity($request);
+	}
+}
