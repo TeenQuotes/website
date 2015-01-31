1c9e73b785c8769a953f302e93c97fe55e7a22e9
Antoine Augusti <antoine@augusti.fr>
2015-01-31 19:49:14 +0100

    Update the script to update environment variables

--- updateEnv.sh
+++ updateEnv.sh
@@ -2,7 +2,3 @@
 # Upload to production
 scp .env root@production.tq:/var/www/website/.env
 ssh root@production.tq "chown www-data:www-data /var/www/website/.env"
-
-# Upload to staging
-scp .env admin@staging.tq:/home/admin/.env
-ssh admin@staging.tq "sudo mv -f /home/admin/.env /var/www/website/.env;sudo chown www-data:www-data /var/www/website/.env"
