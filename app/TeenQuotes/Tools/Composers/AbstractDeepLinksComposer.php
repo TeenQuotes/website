d395fd86b0edab6f4382dac1c50c6032b878dd9a
Antoine Augusti <antoine.augusti@insa-rouen.fr>
2015-02-13 15:37:59 +0100

    Clean the AbstractDeepLinksComposer class

--- app/TeenQuotes/Tools/Composers/AbstractDeepLinksComposer.php
+++ app/TeenQuotes/Tools/Composers/AbstractDeepLinksComposer.php
@@ -1,23 +1,66 @@
 <?php namespace TeenQuotes\Tools\Composers;
 
-use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Lang;
+use Config, Input, Lang;
 
 abstract class AbstractDeepLinksComposer {
 
 	protected function createDeepLinks($path)
 	{
+		$array = [];
+
+		if (Config::get('mobile.iOSApp'))
+			$array = $this->createForIos($array, $path);
+
+		if (Config::get('mobile.androidApp'))
+			$array = $this->createForAndroid($array, $path);
+
+		return $array;
+	}
+
+	/**
+	 * Create deep links data for iOS
+	 *
+	 * @param  array  $data The original data
+	 * @param  string $path The current path
+	 * @return array
+	 */
+	private function createForIos(array $data, $path)
+	{
+		$data['al:ios:url']          = $this->buildUrl($path);
+		$data['al:ios:app_store_id'] = Config::get('mobile.iOSAppID');
+		$data['al:ios:app_name']     = Lang::get('layout.nameWebsite');
+
+		return $data;
+	}
+
+	/**
+	 * Create deep links data for Android
+	 *
+	 * @param  array  $data The original data
+	 * @param  string $path The current path
+	 * @return array
+	 */
+	private function createForAndroid(array $data, $path)
+	{
+		$data['al:android:url']      = $this->buildUrl($path);
+		$data['al:android:app_name'] = Lang::get('layout.nameWebsite');
+		$data['al:android:package']  = Config::get('mobile.androidPackage');
+
+		return $data;
+	}
+
+	/**
+	 * Build a full URL
+	 *
+	 * @param  string $path The current path
+	 * @return string
+	 */
+	private function buildUrl($path)
+	{
 		// If the content is paginated, update the path
 		if (Input::has('page'))
 			$path = $path.'?page='.Input::get('page');
 
-		$array = [
-			'al:ios:url'          => Config::get('mobile.deepLinksProtocol').$path,
-			'al:ios:app_store_id' => Config::get('mobile.iOSAppID'),
-			'al:ios:app_name'     => Lang::get('layout.nameWebsite')
-		];
-
-		return $array;
+		return Config::get('mobile.deepLinksProtocol').$path;
 	}
 }
--- app/config/mobile.php
+++ app/config/mobile.php
@@ -5,13 +5,16 @@ return [
 
 	// Do we have an Android app?
 	'androidApp'        => false,
-	
+
 	// The ID of the iOS application
 	'iOSAppID'          => 'id577239995',
-	
+
+	// The name of package for the Android app
+	'androidPackage'    => '',
+
 	// The URL to download the iOS application
 	'downloadLinkiOS'   => 'https://itunes.apple.com/us/app/teen-quotes/id577239995',
-	
+
 	// Scheme for deep links
 	'deepLinksProtocol' => 'TeenQuotes://',
 ];
