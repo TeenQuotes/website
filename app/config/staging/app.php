3d13458d0632ebffda757bc0402e82d2858c50fa
Antoine Augusti <antoine@augusti.fr>
2015-05-14 17:27:22 +0200

    feat(account): use a custom subdomain for secure actions on account

--- app/config/staging/app.php
+++ app/config/staging/app.php
@@ -1,8 +1,9 @@
 <?php
-return array(
+return [
 	'url'           => 'http://dev.teen-quotes.com',
 	'domain'        => 'dev.teen-quotes.com',
 	'domainAPI'     => 'api.teen-quotes.com',
 	'domainStories' => 'stories.dev.teen-quotes.com',
 	'domainAdmin'   => 'admin.dev.teen-quotes.com',
-);
+	'domainAccount' => 'account.dev.teen-quotes.com',
+];
