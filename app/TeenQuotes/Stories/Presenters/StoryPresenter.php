d0b86df2eabca4b6e2459334a288e96f0d6ca9a6
Antoine Augusti <antoine@augusti.fr>
2015-05-16 12:30:37 +0200

    doc(stories): add documentation for StoryPresenter

--- app/TeenQuotes/Stories/Presenters/StoryPresenter.php
+++ app/TeenQuotes/Stories/Presenters/StoryPresenter.php
@@ -5,22 +5,39 @@ namespace TeenQuotes\Stories\Presenters;
 use App;
 use Laracasts\Presenter\Presenter;
 use Str;
+use TeenQuotes\Quotes\Repositories\QuoteRepository;
 
 class StoryPresenter extends Presenter
 {
+    /**
+     * The diff time telling when the story was published.
+     *
+     * @return string
+     */
     public function storyAge()
     {
         return $this->created_at->diffForHumans();
     }
 
+    /**
+     * Get the total number of quotes published in a human readable way.
+     *
+     * @return string
+     */
     public function totalQuotes()
     {
-        // Round to nearest thousand
-        $quoteRepo = App::make('TeenQuotes\Quotes\Repositories\QuoteRepository');
+        $repo = App::make(QuoteRepository::class);
+        $nbPublished = $repo->totalPublished();
 
-        return number_format(round($quoteRepo->totalPublished(), -3), 0, '.', ',');
+        // Round to nearest thousand
+        return number_format(round($nbPublished, -3), 0, '.', ',');
     }
 
+    /**
+     * Get the page description for a story.
+     *
+     * @return string
+     */
     public function pageDescription()
     {
         return Str::limit($this->frequence_txt, 200);
