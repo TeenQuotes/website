4043514acced8dc6d6ac8c4c901f774c82ab5594
Antoine Augusti <antoine@crowdcube.com>
2015-09-27 07:54:21 +0200

    feat(contact): show a contact form from Helpscout on the contact page

--- app/lang/en/contact.php
+++ app/lang/en/contact.php
@@ -10,17 +10,18 @@
  */
 
 return [
-    'pageTitle'               => 'Contact | '.Lang::get('layout.nameWebsite'),
-    'pageDescription'         => 'Contact the team. Say hi, report a bug or propose a feature!',
-    'contactTitle'            => 'Contact us',
-    'teamTitle'               => 'Our team',
-    'stayInTouchTitle'        => 'Stay in touch',
-    'stayInTouchContent'      => 'We are very concerned by you, our visitors. You are our top priority and this is the reason why we will be very happy to hear from you!<br/><br/>You have an idea? A suggestion? Something seems to be broken on the website? You want to show us your love? You want to drink a beer with us?<br/><br/>We will be very happy to answer to your message. Here are a few informations if you wish to contact us, by hook or by crook (but we do not accept carrier pigeons).',
-    'emailAddress'            => 'support@'.Config::get('app.domain'),
     'chooseYourWeapon'        => 'Choose your weapon',
-    'teamDescriptionClara'    => 'Clara is in charge of the Twitter account. She loves life. Since 2009 she tweets everyday to inspire us! Thanks to her we have build the '.Lang::get('layout.nameWebsite').' web suite.',
+    'contactTitle'            => 'Contact us',
+    'emailAddress'            => 'support@'.Config::get('app.domain'),
+    'helpscoutInfo'           => 'We will get back to you shortly!',
+    'pageDescription'         => 'Contact the team. Say hi, report a bug or propose a feature!',
+    'pageTitle'               => 'Contact | '.Lang::get('layout.nameWebsite'),
+    'stayInTouchContent'      => 'We are very concerned by you, our visitors. You are our top priority and this is the reason why we will be very happy to hear from you!<br/><br/>You have an idea? A suggestion? Something seems to be broken on the website? You want to show us your love? You want to drink a beer with us?<br/><br/>We will be very happy to answer to your message. Here are a few informations if you wish to contact us, by hook or by crook (but we do not accept carrier pigeons).',
+    'stayInTouchTitle'        => 'Stay in touch',
     'teamDescriptionAntoine'  => 'Antoine is the lead developer. He likes when servers respond fast and when everything work as expected. He is always happy to have an impact on your life. He works hard to make things easy for you.',
-    'teamDescriptionMaxime'   => 'Maxime is the iOS developer. Thanks to him you have '.Lang::get('layout.nameWebsite').' in your pocket, one tap away! He hates Antoine because he designs difficult-to-understand systems.',
+    'teamDescriptionClara'    => 'Clara is in charge of the Twitter account. She loves life. Since 2009 she tweets everyday to inspire us! Thanks to her we have build the '.Lang::get('layout.nameWebsite').' web suite.',
     'teamDescriptionJonathan' => 'Jonathan is the designer. Usually, Antoine designs something ugly and Jonathan makes it pretty. If you like the design then you will love Jonathan. And everybody loves Jonathan!',
+    'teamDescriptionMaxime'   => 'Maxime is the iOS developer. Thanks to him you have '.Lang::get('layout.nameWebsite').' in your pocket, one tap away! He hates Antoine because he designs difficult-to-understand systems.',
     'teamDescriptionMichel'   => 'Michel is the tester. If something is not working or could be improved, he will find it for sure. A lot of software work thanks to him!',
+    'teamTitle'               => 'Our team',
 ];
