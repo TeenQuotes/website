f764d2a15847722a64e1e618bf7df61096762104
Antoine Augusti <antoine@crowdcube.com>
2015-11-22 11:30:40 +0100

    Use dedicated class for available routes

--- app/TeenQuotes/AdminPanel/AdminPanelServiceProvider.php
+++ app/TeenQuotes/AdminPanel/AdminPanelServiceProvider.php
@@ -12,6 +12,7 @@
 namespace TeenQuotes\AdminPanel;
 
 use Illuminate\Support\ServiceProvider;
+use TeenQuotes\AdminPanel\Helpers\Moderation;
 
 class AdminPanelServiceProvider extends ServiceProvider
 {
@@ -33,13 +34,15 @@ class AdminPanelServiceProvider extends ServiceProvider
 
     private function registerRoutes()
     {
-        $this->app['router']->pattern('decision', 'approve|unapprove|alert');
+        $this->app['router']->pattern('decision', Moderation::presentAvailableTypes());
 
-        $this->app['router']->group($this->getRouteGroupParams(), function () {
-            $this->app['router']->get('/', ['uses' => $this->getController().'@index', 'as' => 'admin.quotes.index']);
-            $this->app['router']->get('edit/{quote_id}', ['uses' => $this->getController().'@edit', 'as' => 'admin.quotes.edit']);
-            $this->app['router']->put('update/{quote_id}', ['uses' => $this->getController().'@update', 'as' => 'admin.quotes.update']);
-            $this->app['router']->post('moderate/{quote_id}/{decision}', ['uses' => $this->getController().'@postModerate', 'as' => 'admin.quotes.moderate']);
+        $controller = $this->getController();
+
+        $this->app['router']->group($this->getRouteGroupParams(), function () use($controller) {
+            $this->app['router']->get('/', ['uses' => $controller.'@index', 'as' => 'admin.quotes.index']);
+            $this->app['router']->get('edit/{quote_id}', ['uses' => $controller.'@edit', 'as' => 'admin.quotes.edit']);
+            $this->app['router']->put('update/{quote_id}', ['uses' => $controller.'@update', 'as' => 'admin.quotes.update']);
+            $this->app['router']->post('moderate/{quote_id}/{decision}', ['uses' => $controller.'@postModerate', 'as' => 'admin.quotes.moderate']);
         });
     }
 
