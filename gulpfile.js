3fae68876ece24a1ebd6ac0ff9b3e7e70bb204ee
Antoine Augusti <antoine.augusti@gmail.com>
2017-01-01 12:28:04 +0100

    Pull in hotkey.js

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ elixir(function(mix) {
 			'jquery-2.1.0.min.js',
 			'bootstrap.min.js',
 			'mailgun-validator.js',
+			'hotkeys.js',
 			'app.js'
 		], outputJS + 'scripts.min.js', inputJSDir)
 		.version([
