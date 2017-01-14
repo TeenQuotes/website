5469ae0e8b3284599cdf9334057d080cff4c9e68
Antoine Augusti <antoine.augusti@gmail.com>
2017-01-14 16:36:42 +0100

    Enable page-level ads for AdSense

--- ressources/views/layouts/ads/footer.blade.php
+++ ressources/views/layouts/ads/footer.blade.php
@@ -1,17 +1,20 @@
 <div id="ad-footer">
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+	    google_ad_client: "ca-pub-8130906994953193",
+	    enable_page_level_ads: true
+	  });
+	</script>
 	<!-- Footer - TQ v3 -->
 	<ins class="adsbygoogle"
 		 style="display:block"
 		 data-ad-client="ca-pub-8130906994953193"
 		 data-ad-slot="8395382786"
 		 data-ad-format="auto"></ins>
-	<script>
-	(adsbygoogle = window.adsbygoogle || []).push({});
-	</script>
 
 	<!-- Add script to detect ads and show a banner -->
-	@section('add-js')	
+	@section('add-js')
 		{{ HTML::script('assets/js/ads.js') }}
 		{{ HTML::script('assets/js/detect.js') }}
 	@stop
