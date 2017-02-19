7b81a985f7c3af920c3b2aeaf3bb5d79ae92730e
Antoine Augusti <antoine.augusti@gmail.com>
2017-02-19 21:01:30 +0100

    Update Google AdSense ID

--- ressources/views/layouts/ads/footer.blade.php
+++ ressources/views/layouts/ads/footer.blade.php
@@ -1,18 +1,21 @@
 <div id="ad-footer">
 	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<script>
-	  (adsbygoogle = window.adsbygoogle || []).push({
-	    google_ad_client: "ca-pub-8130906994953193",
-	    enable_page_level_ads: true
-	  });
+	(adsbygoogle = window.adsbygoogle || []).push({});
 	</script>
 	<!-- Footer - TQ v3 -->
 	<ins class="adsbygoogle"
 		 style="display:block"
 		 data-ad-client="ca-pub-8130906994953193"
-		 data-ad-slot="8395382786"
+		 data-ad-slot="2387894789"
 		 data-ad-format="auto"></ins>
 
+	<script>
+	  (adsbygoogle = window.adsbygoogle || []).push({
+	    google_ad_client: "ca-pub-8130906994953193",
+	    enable_page_level_ads: true
+	  });
+	</script>
 	<!-- Add script to detect ads and show a banner -->
 	@section('add-js')
 		{{ HTML::script('assets/js/ads.js') }}
